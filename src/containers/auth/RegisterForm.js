ad821854a5e26bc81640571734b16c7c919cf172
dudgp128 <20181048@sungshin.ac.kr>
2023-06-09 17:13:41 +0900

    [FEAT] 회원가입 폼 제출하기

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -2,9 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
 import { changeField, initializeForm } from '../../modules/auth';
 import { useEffect } from 'react';
+import useUserMutation from '../../modules/queries/registerQuery';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
+
+  const mutation = useUserMutation();
+
   const form = useSelector((state) => {
     return state.auth.register;
   });
@@ -17,8 +21,22 @@ const RegisterForm = () => {
     const { value, name } = e.target;
     dispatch(changeField({ form: 'register', key: name, value }));
   };
-  return <AuthForm type="register" form={form} onChange={onChange} />;
-  
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    mutation(username, password);
+  };
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default RegisterForm;
