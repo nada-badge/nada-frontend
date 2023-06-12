7cb46547b165f45aeb9c95355f8add7d9f1afb71
dudgp128 <20181048@sungshin.ac.kr>
2023-06-12 15:01:55 +0900

    [FEAT] 로그인 구현하기

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -2,9 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
 import { changeField, initializeForm } from '../../modules/auth';
 import { useEffect } from 'react';
+import useUserMutation from '../../modules/queries/loginQuery';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
+
+  const { mutate } = useUserMutation();
+
   const form = useSelector((state) => {
     return state.auth.login;
   });
@@ -17,7 +21,21 @@ const LoginForm = () => {
     const { value, name } = e.target;
     dispatch(changeField({ form: 'login', key: name, value }));
   };
-  return <AuthForm type="login" form={form} onChange={onChange} />;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    mutate({ email, password });
+  };
+  return (
+    <AuthForm
+      type="login"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default LoginForm;
