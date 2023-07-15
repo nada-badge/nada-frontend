ab1d47668d3fb35f1e6420da0779289f5c437a6c
dudgp128 <nbv19@naver.com>
2023-07-15 16:49:38 +0900

    [FEAT] React-Native 환경 만들기

---
+++ MySpec/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import WebView from "react-native-webview";
+
+export default function App() {
+  return (
+    <WebView
+      style={styles.container}
+      source={{ uri: "https://docs.expo.dev/versions/latest/sdk/webview/" }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
