d83a61ad5e959991e4b67c60a70cb4122f88e72b
dudgp128 <nbv19@naver.com>
2023-07-23 16:05:59 +0900

    [FEAT] input 이메일 중복 검사하기

---
+++ src/components/auth/EmailInput.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import client from '../../lib/api/client';
+
+const ErrorMessage = styled.div`
+  color: red;
+  text-align: center;
+  font-size: 0.875rem;
+  margin-top: 1rem;
+`;
+
+const EmailInput = ({ form, onChange, checkEmail }) => {
+  const [isBlur, setBlur] = useState(false);
+  const { email } = form;
+
+  const { data, refetch } = useQuery({
+    queryKey: ['getEmail'],
+    queryFn: async () => {
+      const { data } = await client.get('user/checkEmailOverlap', {
+        params: { email: email },
+      });
+      return data;
+    },
+    enabled: isBlur,
+  });
+
+  return (
+    <div>
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={form.email}
+        onBlur={() => {
+          checkEmail();
+          setBlur(true);
+          refetch();
+        }}
+        required
+      />
+      {data && data.result === 0 && (
+        <ErrorMessage>중복된 이메일 입니다.</ErrorMessage>
+      )}
+    </div>
+  );
+};
+export default EmailInput;
