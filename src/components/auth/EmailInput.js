38bdbaa407ea0e9f00ccdce1599a98bbce4140e2
dudgp128 <20181048@sungshin.ac.kr>
2023-07-24 16:20:54 +0900

    [STYLED] 주석문 지우기

--- src/components/auth/EmailInput.js
+++ src/components/auth/EmailInput.js
@@ -1,48 +1,10 @@
-// 회원가입 input email
-import styled from 'styled-components';
-import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
-import client from '../../lib/api/client';
+import AuthTemplate from './AuthTemplate';
 
-const ErrorMessage = styled.div`
-  color: red;
-  text-align: center;
-  font-size: 0.875rem;
-  margin-top: 1rem;
-`;
-
-const EmailInput = ({ email, onChange, checkEmail }) => {
-  const [isBlur, setBlur] = useState(false);
-
-  const { data, refetch } = useQuery({
-    queryKey: ['getEmail'],
-    queryFn: async () => {
-      const { data } = await client.get('user/checkEmailOverlap', {
-        params: { email: email },
-      });
-      return data;
-    },
-    enabled: isBlur,
-  });
-
+const EmailInput = () => {
   return (
-    <div>
-      <input
-        name="email"
-        placeholder="이메일"
-        onChange={onChange}
-        value={email}
-        onBlur={() => {
-          checkEmail();
-          setBlur(true);
-          refetch();
-        }}
-        required
-      />
-      {data && data.result === 0 && (
-        <ErrorMessage>중복된 이메일 입니다.</ErrorMessage>
-      )}
-    </div>
+    <AuthTemplate>
+      <input name="email" placeholder="이메일" required />
+    </AuthTemplate>
   );
 };
 export default EmailInput;
