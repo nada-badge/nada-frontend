a44b41f3d9db14a92f69148367aadf9e0fb8be1a
dudgp128 <nbv19@naver.com>
2023-07-28 16:42:29 +0900

    [FEAT] post- 회원가입 완료하기

--- src/pages/register/PhoneNumberPage.js
+++ src/pages/register/PhoneNumberPage.js
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../components/common/Button';
+import useUserMutation from '../../modules/queries/registerQuery';
+import { changeField } from '../../modules/auth';
 
 const PhoneNumberPage = () => {
+  const dispatch = useDispatch();
+
+  const { mutate } = useUserMutation();
+
   const onBlur = (e) => {
     let { value } = e.target;
+
     if (value.match(/^\d{11}$/)) {
       value =
         value.substr(0, 3) +
@@ -14,10 +22,29 @@ const PhoneNumberPage = () => {
     }
   };
 
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(changeField({ form: 'register', key: name, value }));
+  };
+
+  const register = useSelector(({ auth }) => auth.register);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('useRegister', register);
+    mutate(register);
+  };
+
   return (
     <div>
-      <form>
-        <input name="phoneNumber" placeholder="번호" onBlur={onBlur} required />
+      <form onSubmit={onSubmit}>
+        <input
+          name="phoneNumber"
+          placeholder="번호"
+          onChange={onChange}
+          onBlur={onBlur}
+          required
+        />
         <Button>다음</Button>
       </form>
     </div>
