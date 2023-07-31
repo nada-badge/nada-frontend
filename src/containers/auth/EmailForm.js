5c33e0460e202b07d299ad76322346432ce3007f
dudgp128 <nbv19@naver.com>
2023-07-31 21:38:08 +0900

    [REFACTORY] 입력 페이지 -> 입력 컴포넌트로 나누어, RegisterForm에서 적용하기

---
+++ src/containers/auth/EmailForm.js
@@ -0,0 +1,71 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import client from '../../lib/api/client';
+import { emailSelector } from '../../modules/auth';
+import Button from '../../components/common/Button';
+
+const EmailPage = ({
+  dispatchField,
+  ErrorMessage,
+  errorMessages,
+  onSubmit,
+}) => {
+  const [isBlur, setBlur] = useState(false);
+  const [error, setError] = useState(null);
+
+  const email = useSelector(emailSelector);
+
+  const transientName = useRef(null);
+
+  const checkEmail = () => {
+    const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegexp.test(email)) {
+      setError(errorMessages.email_format);
+    }
+    return emailRegexp.test(email);
+  };
+
+  const { refetch } = useQuery({
+    queryKey: ['getEmail'],
+    queryFn: async () => {
+      const { data } = await client.get('user/checkEmailOverlap', {
+        params: { email: email },
+      });
+      if (!data.result) {
+        setError(errorMessages.email_duplicate);
+      }
+      return data;
+    },
+    enabled: isBlur,
+  });
+
+  const onBlur = () => {
+    if (transientName.current !== email) {
+      transientName.current = email;
+      setError(null);
+      if (checkEmail()) {
+        setBlur(true);
+        refetch();
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={dispatchField}
+        value={email}
+        onBlur={onBlur}
+        required
+        ref={transientName}
+      />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      <Button>비밀번호로</Button>
+    </form>
+  );
+};
+export default EmailPage;
---
+++ src/containers/auth/PhoneNumberForm.js
@@ -0,0 +1,31 @@
+import Button from '../../components/common/Button';
+
+const PhoneNumberPage = ({ dispatchField, onSubmit }) => {
+  const onBlur = (e) => {
+    let { value } = e.target;
+
+    if (value.match(/^\d{11}$/)) {
+      value =
+        value.substr(0, 3) +
+        '-' +
+        value.substr(3, 4) +
+        '-' +
+        value.substr(7, 4);
+      e.target.value = value;
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="phoneNumber"
+        placeholder="번호"
+        onChange={dispatchField}
+        onBlur={onBlur}
+        required
+      />
+      <Button>다음</Button>
+    </form>
+  );
+};
+export default PhoneNumberPage;
