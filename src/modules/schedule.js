d8ac5346207ce5d6c235e6736e8f0323c538dc01
daeun lee <comdang23@gmail.com>
2023-08-01 16:28:16 +0900

    [SYTLE] 주석 추가  #NADA-33

--- src/modules/schedule.js
+++ src/modules/schedule.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialState = { // 1) activity 값 초기화
   activities: {
     _id: '',
     scheduleName: '',      
@@ -21,10 +21,10 @@ const scheduleSlice = createSlice({
   name: 'schedule',
   initialState,
   reducers: {
-    changeField: (state, { payload: { form, value } }) => {      
+    changeField: (state, { payload: { form, value } }) => {      // 2) form와 value를 전달받아 form의 값을 value로 변경
       state[form] = value;
     },
-    initializeForm: (state, { payload: form }) => {
+    initializeForm: (state, { payload: form }) => { // 3) 전달받은 form의 값 초기화
       state[form] = initialState[form];
     },
   },
