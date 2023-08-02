d2a9ccbb72f3c718c773b05b03052c51d9e097ad
dudgp128 <20181048@sungshin.ac.kr>
2023-08-02 15:53:40 +0900

    [RENAME] 파일 정리하기 #NADA-26

---
+++ src/containers/auth/login/LoginForm.js
@@ -0,0 +1,70 @@
+import { useDispatch, useSelector } from 'react-redux';
+import AuthForm from '../../../components/auth/AuthForm';
+import { changeField, initializeForm } from '../../../modules/auth';
+import { useEffect, useState } from 'react';
+import useUserMutation from '../../../modules/queries/loginQuery';
+import { produce } from 'immer';
+import SocialLogin from './SocialLogin';
+import styled from 'styled-components';
+import LoginFooter from './LoginFooter';
+
+const Frame = styled.div`
+  height: 100px;
+  left: 15px;
+  position: absolute;
+  top: 378px;
+  width: 345px;
+`;
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const [error, setError] = useState({
+    email: null,
+    password: null,
+  });
+
+  const { mutate } = useUserMutation();
+
+  const form = useSelector((state) => {
+    return state.auth.login;
+  });
+
+  useEffect(() => {
+    dispatch(initializeForm('login'));
+  }, [dispatch]);
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(changeField({ form: 'login', key: name, value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    setError(
+      produce((draft) => {
+        draft['email'] = email ? null : '이메일을 입력해주세요.';
+        draft['password'] = password ? null : '비밀번호를 입력해주세요.';
+      }),
+    );
+    if (email && password) {
+      mutate({ email, password });
+    }
+  };
+  return (
+    <Frame>
+      <AuthForm
+        form={form}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        error={error}
+      />
+      <SocialLogin />
+      <LoginFooter />
+    </Frame>
+  );
+};
+
+export default LoginForm;
