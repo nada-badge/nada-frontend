eb54a1876da2e0686e391d51a7dbf87f9cc5fbf5
dudgp128 <nbv19@naver.com>
2023-08-02 16:19:32 +0900

    [ADD] 테마(theme) 입히기 #NADA-26

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './modules/auth';
 import scheduleSlice from './modules/schedule';
+import './styles/common.scss';
 
 import Header from './layout/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = configureStore({  
-  reducer: { auth: authSlice.reducer, schedule: scheduleSlice.reducer},
+const store = configureStore({
+  reducer: { auth: authSlice.reducer, schedule: scheduleSlice.reducer },
   devTools: true,
 });
 
