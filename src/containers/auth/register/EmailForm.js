6e88e5b9a05b1b9cfdad38224996256eb460e402
dudgp128 <nbv19@naver.com>
2023-08-02 15:53:40 +0900

    [RENAME] 파일 정리하기 #NADA-26

---
+++ src/containers/auth/register/EmailForm.js
@@ -0,0 +1,63 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import client from '../../../lib/api/client';
+import { emailSelector } from '../../../modules/auth';
+import Button from '../../../components/common/Button';
+import { useCallback } from 'react';
+
+const EmailForm = ({
+  dispatchField,
+  ErrorMessage,
+  errorMessages,
+  onSubmit,
+}) => {
+  const [isBlur, setBlur] = useState(false); // 키보드 포커스 감지, Query문 조건 실행
+  const [error, setError] = useState(null); // error 메세지 관리
+
+  const email = useSelector(emailSelector); // email 상태 가져오기
+
+  // email 유효성 검사
+  const checkEmail = useCallback(() => {
+    const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    setError(emailRegexp.test(email) ? null : errorMessages.email_format);
+    return emailRegexp.test(email);
+  }, [email]);
+
+  // email 중복 검사
+  const { refetch } = useQuery({
+    queryKey: ['getEmail'],
+    queryFn: async () => {
+      const { data } = await client.get('user/checkEmailOverlap', {
+        params: { email: email },
+      });
+      setError(data.result ? null : errorMessages.email_duplicate);
+      return data;
+    },
+    enabled: isBlur,
+  });
+
+  const onBlur = () => {
+    if (checkEmail()) {
+      // 이메일이 유효할때, 중복 검사 진행
+      setBlur(true);
+      refetch();
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={dispatchField}
+        value={email}
+        onBlur={onBlur}
+        required
+      />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      <Button>비밀번호로</Button>
+    </form>
+  );
+};
+export default EmailForm;
---
+++ src/containers/auth/register/PasswordForm.js
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import { produce } from 'immer';
+import { useSelector } from 'react-redux';
+import { passwordSelector } from '../../../modules/auth';
+import Button from '../../../components/common/Button';
+
+const PasswordForm = ({
+  dispatchField,
+  ErrorMessage,
+  onSubmit,
+  errorMessages,
+}) => {
+  const [error, setError] = useState({
+    password: null,
+    passwordConfirm: null,
+  }); // error 메세지 관리하기
+
+  // password 상태 가져오기
+  const { password, passwordConfirm } = useSelector(passwordSelector);
+
+  // 비밀번호 유효성 검사
+  const checkPassword = (value) => {
+    const passwordRegexp =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+    const isValid = passwordRegexp.test(value);
+    setError(
+      produce((draft) => {
+        draft.password = isValid ? null : errorMessages.password;
+      }),
+    );
+    // '비밀번호 확인'에 값이 있으면 '일치'까지 검사하기
+    if (passwordConfirm) {
+      checkPasswordConfirm({ value, password });
+    }
+  };
+
+  // 비밀번호 일치 검사
+  const checkPasswordConfirm = ({ password, value }) => {
+    setError(
+      produce((draft) => {
+        draft.passwordConfirm =
+          password === value ? null : errorMessages.passwordConfirm;
+      }),
+    );
+  };
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    // redux에 상태 반영하기
+    dispatchField(e);
+
+    if (name === 'password') {
+      checkPassword(value); // 비밀번호 유효성 검사
+    }
+    if (name === 'passwordConfirm') {
+      checkPasswordConfirm({ password, value }); // 비밀번호 일치 검사
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="password"
+        name="password"
+        placeholder="비밀번호"
+        onChange={onChange}
+        value={password}
+        required
+      />
+      {error && <ErrorMessage>{error.password}</ErrorMessage>}
+      <input
+        type="password"
+        name="passwordConfirm"
+        placeholder="비밀번호"
+        onChange={onChange}
+        value={passwordConfirm}
+        required
+      />
+      {error && <ErrorMessage>{error.passwordConfirm}</ErrorMessage>}
+      <Button>닉네임으로</Button>
+    </form>
+  );
+};
+export default PasswordForm;
