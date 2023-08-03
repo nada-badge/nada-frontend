38a92d6ee259e339f5d22c1c4e37c98b5b01721e
dudgp128 <20181048@sungshin.ac.kr>
2023-08-03 15:37:00 +0900

    [ADD] 로그인 디자인 입히기 (소셜 로그인) #NADA-26

--- src/containers/auth/login/SocialLogin.js
+++ src/containers/auth/login/SocialLogin.js
@@ -1,28 +1,29 @@
-import './style.css';
+import { Div3, SocialBtn } from '../../../styles/Login';
+
 const SocialLogin = () => {
   return (
-    <div className="div-3">
-      <div className="button-kakao">
-        <div className="div-4">
+    <Div3>
+      <SocialBtn className="kakao">
+        <div>
           <img
-            className="vector"
             alt="Vector"
             src="https://generation-sessions.s3.amazonaws.com/beff57bde8d3f4caef4459378547b83e/img/vector-1.png"
           />
-          <div className="text-wrapper-3">카카오 로그인</div>
+          <div>카카오 로그인</div>
         </div>
-      </div>
-      <div className="button-apple">
-        <div className="div-5">
+      </SocialBtn>
+      <SocialBtn className="apple">
+        <div>
           <img
-            className="img"
+            height="19px"
+            width="15px"
             alt="Vector"
             src="https://generation-sessions.s3.amazonaws.com/beff57bde8d3f4caef4459378547b83e/img/vector.png"
           />
-          <div className="text-wrapper-4">Apple 로그인</div>
+          <div>Apple 로그인</div>
         </div>
-      </div>
-    </div>
+      </SocialBtn>
+    </Div3>
   );
 };
 
