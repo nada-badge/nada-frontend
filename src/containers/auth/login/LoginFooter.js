bda8a61657a1d8afc82f5213dd732e6f5d993b97
dudgp128 <nbv19@naver.com>
2023-08-03 16:30:34 +0900

    [ADD] 로그인 디자인 입히기 (LoginFooter) #NADA-26

--- src/containers/auth/login/LoginFooter.js
+++ src/containers/auth/login/LoginFooter.js
@@ -1,28 +1,26 @@
-import './style.css';
 import { Link } from 'react-router-dom';
+import { Div6 } from '../../../styles/Login';
 
 const LoginFooter = () => {
   return (
-    <div className="div-6">
-      <div className="view">
-        <Link className="text-wrapper-5" to="/register">
-          회원 가입하기
-        </Link>
+    <Div6>
+      <div>
+        <Link to="/register">회원 가입하기</Link>
         <img
           className="vector-2"
           alt="Vector"
           src="https://generation-sessions.s3.amazonaws.com/beff57bde8d3f4caef4459378547b83e/img/vector-2-1.svg"
         />
       </div>
-      <div className="view-2">
-        <div className="text-wrapper-6">아이디/비밀번호 찾기</div>
+      <div className="findId">
+        <Link>아이디/비밀번호 찾기</Link>
         <img
           className="vector-2"
           alt="Vector"
           src="https://generation-sessions.s3.amazonaws.com/beff57bde8d3f4caef4459378547b83e/img/vector-2-1.svg"
         />
       </div>
-    </div>
+    </Div6>
   );
 };
 
