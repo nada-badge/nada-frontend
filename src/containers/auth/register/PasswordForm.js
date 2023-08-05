3a0856767b826f4281053471047611742e8be6b5
dudgp128 <20181048@sungshin.ac.kr>
2023-08-05 12:20:02 +0900

    [FEAT] 비밀번호 조건 부합 여부에 따라 체크 리스트 변경하기

--- src/containers/auth/register/PasswordForm.js
+++ src/containers/auth/register/PasswordForm.js
@@ -14,11 +14,11 @@ import {
 const PasswordForm = ({ dispatchField, onSubmit, order }) => {
   const [check, setCheck] = useState({
     password: {
-      length: '',
-      text: '',
-      special: '',
+      length: false,
+      text: false,
+      special: false,
     },
-    passwordConfirm: '',
+    passwordConfirm: false,
   }); // error 메세지 관리하기
 
   // password 상태 가져오기
@@ -28,11 +28,11 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
   const checkPassword = (value) => {
     setCheck(
       produce((draft) => {
-        draft.password.length = value.length > 8 && value.length < 16;
-        draft.password.text = value.match(/^(?=.*[a-zA-Z])(?=.*[0-9])/);
-        draft.password.special = value.match(
-          /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]/,
-        );
+        draft.password.length = value.length >= 8 && value.length <= 16;
+        const textRegex = new RegExp(/(?=.*[a-zA-Z])(?=.*[0-9])/);
+        const specialRegex = new RegExp(/[!@#$%^&*]/);
+        draft.password.text = textRegex.test(value);
+        draft.password.special = specialRegex.test(value);
       }),
     );
     // '비밀번호 확인'에 값이 있으면 '일치'까지 검사하기
@@ -45,7 +45,7 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
   const checkPasswordConfirm = ({ password, value }) => {
     setCheck(
       produce((draft) => {
-        draft.passwordConfirm = password === value ? null : false;
+        draft.passwordConfirm = password === value;
       }),
     );
   };
@@ -86,27 +86,51 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
           </InputWrapper>
           <CheckListBox>
             <CheckList $position>
-              <img
-                className="check"
-                alt="Check"
-                src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-              />
+              {check.password.length ? (
+                <img
+                  className="check"
+                  alt="Check"
+                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
+                />
+              ) : (
+                <img
+                  className="check"
+                  alt="Check"
+                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
+                />
+              )}
               <ErrorMessage>8-16자</ErrorMessage>
             </CheckList>
             <CheckList $position={{ left: 80 }}>
-              <img
-                className="check"
-                alt="Check"
-                src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-              />
+              {check.password.text ? (
+                <img
+                  className="check"
+                  alt="Check"
+                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
+                />
+              ) : (
+                <img
+                  className="check"
+                  alt="Check"
+                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
+                />
+              )}
               <ErrorMessage>영문 대, 소문자 포함</ErrorMessage>
             </CheckList>
             <CheckList $position={{ left: 240 }}>
-              <img
-                className="check"
-                alt="Check"
-                src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-              />
+              {check.password.special ? (
+                <img
+                  className="check"
+                  alt="Check"
+                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
+                />
+              ) : (
+                <img
+                  className="check"
+                  alt="Check"
+                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
+                />
+              )}
               <ErrorMessage>특수문자 포함</ErrorMessage>
             </CheckList>
           </CheckListBox>
@@ -124,11 +148,19 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
             </InputWrapper>
             <div>
               <CheckList $position={{ top: 165 }}>
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-                />
+                {check.passwordConfirm ? (
+                  <img
+                    className="check"
+                    alt="Check"
+                    src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
+                  />
+                ) : (
+                  <img
+                    className="check"
+                    alt="Check"
+                    src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
+                  />
+                )}
                 <ErrorMessage>비밀번호 일치</ErrorMessage>
               </CheckList>
             </div>
