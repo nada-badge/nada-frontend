da69926417ab998bf04487ddbdd8cd7f00cd88a9
daeun lee <comdang23@gmail.com>
2023-08-05 18:23:09 +0900

    [REFACT]client 주소 수정 #NADA-33

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const client = axios.create();
 
-client.defaults.baseURL = 'http://34.64.104.214:3000';
+client.defaults.baseURL = 'http://3.36.18.249:9553';
 
 export default client;
