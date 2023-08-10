65b2f84aa1c4de0db873fe7f8c9543a6ce363495
dudgp128 <20181048@sungshin.ac.kr>
2023-08-10 17:17:21 +0900

    [FIX] 디자인 - 버튼 중앙 배열 맞추기 #NADA-26

--- src/pages/auth/LoginSelect.js
+++ src/pages/auth/LoginSelect.js
@@ -2,6 +2,15 @@ import React from 'react';
 import AuthTemplate from '../../components/auth/AuthTemplate';
 import { LoginBox, Logo, LoginBtn } from '../../styles/Login';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LinkBox = styled.div`
+  height: 140px;
+  left: 15px;
+  position: absolute;
+  top: 576px;
+  width: 345px;
+`;
 
 const LogInSelect = () => {
   return (
@@ -11,16 +20,18 @@ const LogInSelect = () => {
           alt="Logo"
           src="https://generation-sessions.s3.amazonaws.com/dccd720493d3d99169fdd13158e459fa/img/logo.svg"
         />
-        <Link to="/login/personal">
-          <LoginBtn $position={{ top: 576 }}>
-            <div>개인 회원</div>
-          </LoginBtn>
-        </Link>
-        <Link to="/login/team">
-          <LoginBtn $position={{ top: 652 }} className="team">
-            <div>단체 회원</div>
-          </LoginBtn>
-        </Link>
+        <LinkBox>
+          <Link to="/login/personal">
+            <LoginBtn $position>
+              <div>개인 회원</div>
+            </LoginBtn>
+          </Link>
+          <Link to="/login/team">
+            <LoginBtn $position={{ top: 76 }} className="team">
+              <div>단체 회원</div>
+            </LoginBtn>
+          </Link>
+        </LinkBox>
       </LoginBox>
     </AuthTemplate>
   );
