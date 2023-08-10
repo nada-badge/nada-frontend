6641856627f53cb355a09e4fee43b883eb413180
daeun lee <comdang23@gmail.com>
2023-08-10 15:21:39 +0900

    [FEAT] 게시물 삭제 기능 추가 NADA-33

--- src/routes/BoardDetail.js
+++ src/routes/BoardDetail.js
@@ -1,10 +1,41 @@
 /* BoardDetail.js */
 import Board from '../components/board/Board';
+import { useDispatch, useSelector } from 'react-redux';
+import { initializeForm } from '../modules/schedule';
+import { useNavigate } from 'react-router-dom';
+import { idSelector } from '../modules/schedule';
+import client from '../lib/api/client';
+
 const BoardDetail = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const id = useSelector(idSelector);
+
+  const initialize = () => {
+    dispatch(initializeForm('activities'));
+  }
+
+  const DeleteAct = () => {
+    
+    console.log("id :", id)
+    client.delete('/schedule', {data :{_id : id}});
+    
+    initialize();
+    alert('삭제되었습니다.');
+    navigate('/board');
+    
+  }
+
+ 
 
   return (
     <div>
+    <div>
         <Board/>
+    </div>
+    <button onClick={DeleteAct}>수정</button>
+    <button onClick={DeleteAct}>삭제</button>
     </div>
   );
 };
