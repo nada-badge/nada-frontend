797c82f20b6de4f5f0c86165aecbe24ccc76ce34
daeun lee <comdang23@gmail.com>
2023-08-10 15:14:49 +0900

    [REFACT] createSelector를 사용 #NADA-33

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,26 +1,14 @@
 /* Board.js 상세보기에 해당하는 데이터를 받아 화면에 출력할 컴포넌트*/
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { activitySelector } from '../../modules/schedule';
 
 console.log("board가 실행되었습니다.")
 const Board = () => {
 
   // 1) useSelector 를 통해 클릭된 활동의 세부 값을 shcedule에서 board로 받아온다.
-  const { scheduleName, groupId, groupName, field, category, area, content, startedAt, endedAt, registeredAt, updatedAt } = useSelector(({ schedule }) => ({
-    scheduleName: schedule.activities.scheduleName,
-    groupId: schedule.activities.groupId,
-    groupName: schedule.activities.groupName,
-    field: schedule.activities.field,
-    category: schedule.activities.category,
-    area: schedule.activities.area,
-    content: schedule.activities.content,
-    startedAt: schedule.activities.startedAt,
-    endedAt: schedule.activities.endedAt,
-    registeredAt: schedule.activities.registeredAt,
-    updatedAt: schedule.activities.updatedAt
-  }));
-
-
+  const { scheduleName, groupId, groupName, field, category, area, content, startedAt, endedAt, registeredAt, updatedAt } = useSelector( activitySelector ) 
+  
 
   return (
     <div>
--- src/modules/schedule.js
+++ src/modules/schedule.js
@@ -1,10 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
 
 const initialState = { // 1) activity 값 초기화
   activities: {
     _id: '',
-    scheduleName: '',      
-    groupId: '',      
+    scheduleName: '',          
     groupName: '',    
     field: '',      
     category: '',      
@@ -30,5 +29,31 @@ const scheduleSlice = createSlice({
   },
 });
 
+const idSelect = (rootState) => rootState.schedule.activities._id || initialState.activities._id;
+const scheduleNameSelect = (rootState) => rootState.schedule.activities.scheduleName || initialState.activities.scheduleName;
+const groupNameSelect = (rootState) => rootState.schedule.activities.groupName || initialState.activities.groupName;
+const fieldSelect = (rootState) => rootState.schedule.activities.field || initialState.activities.field;
+const categorySelect = (rootState) => rootState.schedule.activities.category || initialState.activities.category;
+const areaSelect = (rootState) => rootState.schedule.activities.area || initialState.activities.area;
+const contentSelect = (rootState) => rootState.schedule.activities.content || initialState.activities.content;
+const startedAtSelect = (rootState) => rootState.schedule.activities.startedAt || initialState.activities.startedAt;
+const endedAtSelect = (rootState) => rootState.schedule.activities.endedAt || initialState.activities.endedAt;
+const registeredAtSelect = (rootState) => rootState.schedule.activities.registeredAt || initialState.activities.registeredAt;
+const updatedAtSelect = (rootState) => rootState.schedule.activities.updatedAt || initialState.activities.updatedAt;
+
+export const idSelector = createSelector(idSelect, (_id) => _id);
+export const activitySelector = createSelector(
+  scheduleNameSelect,
+  groupNameSelect,
+  fieldSelect,
+  categorySelect,
+  areaSelect,
+  contentSelect,
+  startedAtSelect,
+  endedAtSelect,
+  registeredAtSelect,
+  updatedAtSelect,
+  (scheduleName,groupName,field,category,area,content,startedAt,endedAt,registeredAt,updatedAt) => 
+  ({ scheduleName,groupName,field,category,area,content,startedAt,endedAt,registeredAt,updatedAt }),);
 export default scheduleSlice;
 export const { changeField, initializeForm } = scheduleSlice.actions;
