37e904abb0ce61c3776164090d1ac0d41da7e994
dudgp128 <20181048@sungshin.ac.kr>
2023-08-11 20:42:30 +0900

    [FEAT] 주소에 따라 개인/단체 회원가입 화면 보여주기 #NADA-26

--- src/pages/auth/RegisterPage.js
+++ src/pages/auth/RegisterPage.js
@@ -1,11 +1,24 @@
+import { useState, useEffect } from 'react';
 import AuthTemplate from '../../components/auth/AuthTemplate';
 import RegisterForm from '../../containers/auth/register/RegisterForm';
+import { useLocation } from 'react-router-dom';
 
+const registerMap = {
+  '/register/personal': 'personal',
+  '/register/team': 'team',
+};
 
 const RegisterPage = () => {
+  const location = useLocation();
+  const [type, setType] = useState(null);
+
+  useEffect(() => {
+    setType(registerMap[location.pathname]);
+  }, [location.pathname]);
+
   return (
     <AuthTemplate>
-      <RegisterForm />
+      <RegisterForm type={type} />
     </AuthTemplate>
   );
 };
