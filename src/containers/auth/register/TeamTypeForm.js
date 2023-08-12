1ae8b320a1da16a0b20a624be4a6abf4881d7276
dudgp128 <20181048@sungshin.ac.kr>
2023-08-12 22:46:54 +0900

    [FEAT] 개인/회원에 따라 다른 회원가입 form List 설정하기 #NADA-26

---
+++ src/containers/auth/register/TeamTypeForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Title } from '../../../styles/Register';
+import TeamTypeList from '../../../components/auth/TeamList/TeamTypeList';
+
+export const TeamTypeForm = () => {
+  return (
+    <div>
+      <Title>
+        <h1>
+          단체 종류를
+          <br />
+          선택해 주세요
+        </h1>
+      </Title>
+      <TeamTypeList />
+    </div>
+  );
+};
