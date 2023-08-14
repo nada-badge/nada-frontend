31c9b69521ac50fbad91420239efc1f25ae2a610
dudgp128 <20181048@sungshin.ac.kr>
2023-08-14 14:09:57 +0900

    [REFACT] CheckList 컴포넌트 분리하기 #NADA-26
    PaawordForm, UserNameForm

--- src/containers/auth/register/PasswordForm.js
+++ src/containers/auth/register/PasswordForm.js
@@ -2,25 +2,17 @@ import { useState, useEffect, useCallback } from 'react';
 import { produce } from 'immer';
 import { useSelector } from 'react-redux';
 import { passwordSelector } from '../../../modules/auth';
-import {
-  Title,
-  InputWrapper,
-  Form,
-  CheckListBox,
-  CheckList,
-  ErrorMessage,
-  ButtonBox,
-} from '../../../styles/Register';
-
+import { Title, InputWrapper, Form, ButtonBox } from '../../../styles/Register';
+import CheckList from '../../../components/auth/CheckList/CheckList';
 const PasswordForm = ({ dispatchField, onSubmit, order }) => {
   // error 메세지 관리하기
   const [check, setCheck] = useState({
-    password: {
-      length: false,
-      text: false,
-      special: false,
-    },
-    passwordConfirm: false,
+    password: [
+      { id: 'Password_length', text: '8-16자', checked: false },
+      { id: 'Password_words', text: '영문 대/소문자, 숫자', checked: false },
+      { id: 'Password_special', text: '특수문자 포함', checked: false },
+    ],
+    passwordConfirm: [{ id: 'same', text: '비밀번호 일치', checked: false }],
   });
   // 버튼 투명도, 비활성화 상태 관리
   const [opacity, setOpacity] = useState(0.3);
@@ -32,11 +24,12 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
   const checkPassword = useCallback(() => {
     setCheck(
       produce((draft) => {
-        draft.password.length = password.length >= 8 && password.length <= 16;
-        const textRegex = new RegExp(/(?=.*[a-zA-Z])(?=.*[0-9])/);
+        draft.password[0].checked =
+          password.length >= 8 && password.length <= 16;
+        const wordsRegex = new RegExp(/(?=.*[a-zA-Z])(?=.*[0-9])/);
         const specialRegex = new RegExp(/[!@#$%^&*]/);
-        draft.password.text = textRegex.test(password);
-        draft.password.special = specialRegex.test(password);
+        draft.password[1].checked = wordsRegex.test(password);
+        draft.password[2].checked = specialRegex.test(password);
       }),
     );
     // '비밀번호 확인'에 값이 있으면 '일치'까지 검사하기
@@ -50,7 +43,7 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
   const checkPasswordConfirm = () => {
     setCheck(
       produce((draft) => {
-        draft.passwordConfirm =
+        draft.passwordConfirm[0].checked =
           password === '' ? false : password === passwordConfirm;
       }),
     );
@@ -65,9 +58,12 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
 
   // check 상태 변경시, 버튼 투명도, 활성화 상태 업데이트
   useEffect(() => {
-    const { length, text, special } = check.password;
-    const isValid = length && text && special && check.passwordConfirm;
-    setOpacity(isValid ? 1 : 0.3);
+    const isAllPasswordChecked = check.password.reduce(
+      (acc, current) => acc && current.checked,
+      true,
+    );
+    const isPasswordConfirmChecked = check.passwordConfirm[0].checked;
+    setOpacity(isAllPasswordChecked && isPasswordConfirmChecked ? 1 : 0.3);
   }, [check]);
 
   // 닉네임에서 뒤로가기시, 이전 비밀번호와 check값과 동일하게
@@ -75,9 +71,12 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
     if (password && passwordConfirm) {
       checkPassword(password);
       checkPasswordConfirm({ password, passwordConfirm });
-      const { length, text, special } = check.password;
-      const isValid = length && text && special && check.passwordConfirm;
-      setOpacity(isValid ? 1 : 0.3);
+      const isAllPasswordChecked = check.password.reduce(
+        (acc, current) => acc && current.checked,
+        true,
+      );
+      const isPasswordConfirmChecked = check.passwordConfirm[0].checked;
+      setOpacity(isAllPasswordChecked && isPasswordConfirmChecked ? 1 : 0.3);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -103,56 +102,7 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
               required
             />
           </InputWrapper>
-          <CheckListBox>
-            <CheckList $position>
-              {check.password.length ? (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
-                />
-              ) : (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-                />
-              )}
-              <ErrorMessage>8-16자</ErrorMessage>
-            </CheckList>
-            <CheckList $position={{ left: 80 }}>
-              {check.password.text ? (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
-                />
-              ) : (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-                />
-              )}
-              <ErrorMessage>영문 대, 소문자 포함</ErrorMessage>
-            </CheckList>
-            <CheckList $position={{ left: 240 }}>
-              {check.password.special ? (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
-                />
-              ) : (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-                />
-              )}
-              <ErrorMessage>특수문자 포함</ErrorMessage>
-            </CheckList>
-          </CheckListBox>
+          <CheckList list={check.password} />
         </div>
         <div>
           <InputWrapper $position={{ top: 109 }}>
@@ -165,24 +115,7 @@ const PasswordForm = ({ dispatchField, onSubmit, order }) => {
               required
             />
           </InputWrapper>
-          <CheckListBox>
-            <CheckList $position={{ top: 165 }}>
-              {check.passwordConfirm ? (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/38b27749be3d915368d8e6ccd4ebc802/img/check-2.svg"
-                />
-              ) : (
-                <img
-                  className="check"
-                  alt="Check"
-                  src="https://generation-sessions.s3.amazonaws.com/a978d180a1bb500ecf9724a4add382cf/img/check-3.svg"
-                />
-              )}
-              <ErrorMessage>비밀번호 일치</ErrorMessage>
-            </CheckList>
-          </CheckListBox>
+          <CheckList list={check.passwordConfirm} />
         </div>
       </Form>
       <div>
