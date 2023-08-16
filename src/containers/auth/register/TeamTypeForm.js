489f0e58b87ba44fdf81a9cb4141793049b710c8
dudgp128 <nbv19@naver.com>
2023-08-16 11:42:43 +0900

    [FEAT] 클릭시 이벤트를 감지하고,해당 id의 checked 값 변경 및 나머지 team의 checked값 false로 수정하기

--- src/containers/auth/register/TeamTypeForm.js
+++ src/containers/auth/register/TeamTypeForm.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import TeamTypeList from '../../../components/auth/TeamType/TeamTypeList';
 import Title from '../../../components/auth/Title';
 import { Form } from '../../../styles/Register';
 import Button from '../../../components/auth/Button';
 
 export const TeamTypeForm = ({ order, onSubmit }) => {
+  const [teamType, setTeamType] = useState([
+    { id: 11, text: '교내 동아리', checked: false },
+    { id: 12, text: '연합 동아리', checked: false },
+    { id: 13, text: '학회', checked: false },
+    { id: 14, text: '학과', checked: false },
+    { id: 15, text: '학생회', checked: false },
+    { id: 21, text: '공공기관', checked: false },
+    { id: 22, text: '사기업', checked: false },
+  ]);
+
+  const onClick = useCallback(
+    (id) => {
+      setTeamType(
+        teamType.map((team) =>
+          id === team.id
+            ? { ...team, checked: !team.checked }
+            : { ...team, checked: false },
+        ),
+      );
+    },
+    [teamType],
+  );
+
   return (
     <div>
       <Title text={'단체 종류를'} />
       <Form id={order} onSubmit={onSubmit}>
-        <TeamTypeList />
+        <TeamTypeList teamType={teamType} onClick={onClick} />
       </Form>
       <Button
         form={order}
