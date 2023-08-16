5408596e8565d6f317d909c5858c205f2548a4de
dudgp128 <20181048@sungshin.ac.kr>
2023-08-16 10:50:24 +0900

    [STYLED] TeamTypde의 Form 요소 components->containers로 옮기기 #NADA-26

--- src/containers/auth/register/TeamTypeForm.js
+++ src/containers/auth/register/TeamTypeForm.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import TeamTypeList from '../../../components/auth/TeamType/TeamTypeList';
 import Title from '../../../components/auth/Title';
+import { Form } from '../../../styles/Register';
+import Button from '../../../components/auth/Button';
 
-export const TeamTypeForm = () => {
+export const TeamTypeForm = ({ order, onSubmit }) => {
   return (
     <div>
       <Title text={'단체 종류를'} />
-      <TeamTypeList />
+      <Form id={order} onSubmit={onSubmit}>
+        <TeamTypeList />
+      </Form>
+      <Button
+        form={order}
+        text={'다음'}
+        style={{ position: 'relative', top: '30px' }}
+      />
     </div>
   );
 };
