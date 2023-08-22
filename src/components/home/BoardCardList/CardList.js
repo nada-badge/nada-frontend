1195206981f6701a7fb2af6138509b065eb890f6
dudgp128 <20181048@sungshin.ac.kr>
2023-08-22 17:38:07 +0900

    [REFACT] 'CardList' 컴포넌트 통일(재사용) 시키기 전에 각 요소 스타일 확인 #NADA-43

--- src/components/home/BoardCardList/CardList.js
+++ src/components/home/BoardCardList/CardList.js
@@ -4,11 +4,13 @@ import CardItem from './CardItem';
 const CardsContainer = styled.div`
   background-color: var(--myspec-gray-scalewhite);
   box-shadow: 0px 2px 5px #00000005;
-  height: 212px;
-  left: 0;
   position: relative;
+  margin: 12px 0px;
+
+  /* height: 212px; */
+  /* left: 0; */
   /* top: 434px; */
-  width: 375px;
+  /* width: 375px; */
 
   & > h1 {
     color: #000000;
@@ -19,27 +21,30 @@ const CardsContainer = styled.div`
     left: 16px;
     letter-spacing: var(--subtitle-01-letter-spacing);
     line-height: var(--subtitle-01-line-height);
-    position: absolute;
-    top: 11px;
+    position: relative;
     white-space: nowrap;
+    padding: 16px 0px;
+    margin: 0;
+    /* top: 11px; */
   }
 `;
 
 const CardListWrapper = styled.div`
+  gap: 12px;
   align-items: flex-start;
   display: flex;
-  gap: 12px;
   left: 15px;
   overflow-x: scroll;
-  position: absolute;
-  top: 52px;
-  width: 345px;
+  position: relative;
+  padding-bottom: 12px;
+  /* top: 52px; */
+  /* width: 345px; */
 `;
 
 const cards = [
-  { id: 1, title: '유용한 활동 사이..', category: '자유' },
-  { id: 2, title: '같이 공모전 나가…', category: '홍보' },
-  { id: 3, title: '팀원 모집합니다…', category: '홍보' },
+  { id: 1, title: '유용한 활동 사이트', category: '자유' },
+  { id: 2, title: '같이 공모전 나가실 분', category: '홍보' },
+  { id: 3, title: '팀원 모집합니다.', category: '홍보' },
 ];
 
 const CardList = () => {
