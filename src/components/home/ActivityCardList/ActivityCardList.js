697de37334c20a9fcfcb1abd4cfa4798c25f634a
dudgp128 <nbv19@naver.com>
2023-08-22 17:47:09 +0900

    [REFACT] 'CardList' 컴포넌트 분리 및 통일하기 #NADA-43

--- src/components/home/ActivityCardList/ActivityCardList.js
+++ src/components/home/ActivityCardList/ActivityCardList.js
@@ -1,45 +1,5 @@
-import styled from 'styled-components';
+import { CardsContainer, CardListWrapper } from '../CardList';
 import ActivityItem from './ActivityItem';
-
-const CardsContainer = styled.div`
-  background-color: var(--myspec-gray-scalewhite);
-  box-shadow: 0px 2px 5px #00000005;
-  position: relative;
-  margin: 12px 0px;
-
-  /* height: 277px; */
-  /* left: 0; */
-  /* overflow-x: clip; */
-  /* top: 658px; */
-  /* width: 375px; */
-
-  & > h1 {
-    color: #000000;
-    font-family: var(--subtitle-01-font-family);
-    font-size: var(--subtitle-01-font-size);
-    font-style: var(--subtitle-01-font-style);
-    font-weight: var(--subtitle-01-font-weight);
-    left: 16px;
-    letter-spacing: var(--subtitle-01-letter-spacing);
-    line-height: var(--subtitle-01-line-height);
-    position: relative;
-    white-space: nowrap;
-    padding: 16px 0px;
-    margin: 0;
-    /* top: 11px; */
-  }
-`;
-
-const CardListWrapper = styled.div`
-  gap: 8px;
-  align-items: flex-start;
-  display: flex;
-  left: 15px;
-  overflow-x: scroll;
-  position: relative;
-  padding-bottom: 12px;
-  /* top: 52px; */
-`;
 
 const cards = [
   { id: 1, title: '2023년 성북구 청년 소셜 벤처 혁신 경연대회', Dday: 7 },
--- src/components/home/BoardCardList/CardList.js
+++ src/components/home/BoardCardList/CardList.js
@@ -1,45 +1,5 @@
-import styled from 'styled-components';
+import { CardsContainer, CardListWrapper } from '../CardList';
 import CardItem from './CardItem';
-
-const CardsContainer = styled.div`
-  background-color: var(--myspec-gray-scalewhite);
-  box-shadow: 0px 2px 5px #00000005;
-  position: relative;
-  margin: 12px 0px;
-
-  /* height: 212px; */
-  /* left: 0; */
-  /* top: 434px; */
-  /* width: 375px; */
-
-  & > h1 {
-    color: #000000;
-    font-family: var(--subtitle-01-font-family);
-    font-size: var(--subtitle-01-font-size);
-    font-style: var(--subtitle-01-font-style);
-    font-weight: var(--subtitle-01-font-weight);
-    left: 16px;
-    letter-spacing: var(--subtitle-01-letter-spacing);
-    line-height: var(--subtitle-01-line-height);
-    position: relative;
-    white-space: nowrap;
-    padding: 16px 0px;
-    margin: 0;
-    /* top: 11px; */
-  }
-`;
-
-const CardListWrapper = styled.div`
-  gap: 12px;
-  align-items: flex-start;
-  display: flex;
-  left: 15px;
-  overflow-x: scroll;
-  position: relative;
-  padding-bottom: 12px;
-  /* top: 52px; */
-  /* width: 345px; */
-`;
 
 const cards = [
   { id: 1, title: '유용한 활동 사이트', category: '자유' },
