363dc2f230e8448e2e1717d982d13ae19e782e44
dudgp128 <nbv19@naver.com>
2023-08-24 15:32:52 +0900

    [FEAT] Splash 화면 구하기 ##NADA-47

--- MySpec/App.js
+++ MySpec/App.js
@@ -1,8 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WebView from "react-native-webview";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync()
+  .then((result) =>
+    console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`)
+  )
+  .catch(console.warn);
 
 export default function App() {
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 5000);
+  }, []);
+
   return (
     <WebView
       style={styles.container}
