7d61a4b299e7e5826490968db677a0e898f20467
daeun lee <comdang23@gmail.com>
2023-08-25 10:22:52 +0900

    [ADD] 하단/상단 바 figma css 적용 #NADA-37

--- navigation/src/modules/headerStatus.js
+++ navigation/src/modules/headerStatus.js
@@ -2,11 +2,8 @@ import { createSlice, createSelector } from '@reduxjs/toolkit';
 
 const initialState = { // 1) activity 값 초기화
   headerStatus: {
-    logoStatus: '',
-    backStatus: '',          
-    pageNameStatus: '메롱메롱',    
-    alarmStatus: '',      
-    menuStatus: '',         
+    pageStatus: 'funtion-only',         
+    pageNameStatus: '메롱메롱',          
   },
 };
 
@@ -14,8 +11,8 @@ const headerStatusSlice = createSlice({
   name: 'headerStatus',
   initialState,
   reducers: {
-    changeField: (state, { payload: { form, value } }) => {      // 2) form와 value를 전달받아 form의 값을 value로 변경
-      state[form] = value;
+    changeField: (state, { payload: { key, value } }) => {
+      state[key] = value;
     },
     initializeForm: (state, { payload: form }) => { // 3) 전달받은 form의 값 초기화
       state[form] = initialState[form];
@@ -24,23 +21,18 @@ const headerStatusSlice = createSlice({
 });
 
 //4)createSelector를 위한 준비
-const logoStatusSelect = (rootState) => rootState.headerStatus.headerStatus.logoStatus || initialState.headerStatus.logoStatus;
-const backStatusSelect = (rootState) => rootState.headerStatus.headerStatus.backStatus || initialState.headerStatus.backStatus;
+const pageStatusSelect = (rootState) => rootState.headerStatus.headerStatus.pageStatus || initialState.headerStatus.pageStatus;
 const pageNameStatusSelect = (rootState) => rootState.headerStatus.headerStatus.pageNameStatus || initialState.headerStatus.pageNameStatus;
-const alarmStatusSelect = (rootState) => rootState.headerStatus.headerStatus.alarmStatus || initialState.headerStatus.alarmStatus;
-const menuStatusSelect = (rootState) => rootState.headerStatus.headerStatus.menuStatus || initialState.headerStatus.menuStatus;
+
 
 
 
 //5) 리렌더링 시 함수의 불필요한 실행을 방지하기 위해 createSelector를 사용
 export const headerStatusSelector = createSelector(
-    logoStatusSelect,
-    backStatusSelect,
+    pageStatusSelect,
     pageNameStatusSelect,
-    alarmStatusSelect,
-    menuStatusSelect,
-  (logoStatus, backStatus, pageNameStatus, alarmStatus, menuStatus) => 
-  ({ logoStatus, backStatus, pageNameStatus, alarmStatus, menuStatus }),);
+  (pageStatus, pageNameStatus) => 
+  ({ pageStatus, pageNameStatus }),);
 
 export default headerStatusSlice;
 export const { changeField, initializeForm } = headerStatusSlice.actions;
--- navigation/src/testpage/home.js
+++ navigation/src/testpage/home.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { changeField } from "../modules/headerStatus";
 
-const home = () => {
+const Home = () => {
+
+    const dispatch = useDispatch();
+    console.log("home 이 실행되었습니다.");
+    const test = "pageNameStatus";
+
+    useEffect(()=>{
+        
+        dispatch(changeField({key : test, value: '테스트' }));
+        console.log("changeField가 실행되었습니다.")
+
+    })
+
+
     return(
         <div>
             home 입니다.
@@ -9,5 +24,5 @@ const home = () => {
     
     }
     
-export default home;
+export default Home;
     
