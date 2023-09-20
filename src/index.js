e686071bf7cfe027cedce3845b472f696b9f5c38
dudgp128 <20181048@sungshin.ac.kr>
2023-09-20 16:39:39 +0900

    [FEAT] redux store calendar 반영하기 #NADA-50

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './modules/auth';
 import scheduleSlice from './modules/schedule';
+import calendarSlice from './modules/calendar';
 import './styles/common.scss';
 import { Mobile, PC } from './styles/Responsive';
-
-import Header from './layout/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const store = configureStore({
-  reducer: { auth: authSlice.reducer, schedule: scheduleSlice.reducer },
+  reducer: {
+    auth: authSlice.reducer,
+    schedule: scheduleSlice.reducer,
+    calendar: calendarSlice.reducer,
+  },
   devTools: true,
 });
 
@@ -23,11 +26,9 @@ root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Mobile>
-        <Header />
         <App />
       </Mobile>
       <PC>
-        <Header />
         <App />
       </PC>
     </BrowserRouter>
