1e046e251d64c15485882cf0f187d11423130c4b
dudgp128 <20181048@sungshin.ac.kr>
2023-09-23 00:46:05 +0900

    [ADD] FullCalendar 커스텀 컴포넌트로 today, event 적용하기 #NADA-43

--- src/containers/calendar/WeekCalendar.js
+++ src/containers/calendar/WeekCalendar.js
@@ -1,6 +1,9 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import './WeekCalendar.scss';
+import '../../styles/Calendar.scss';
+import TodayBox from '../../components/calendar/today';
+import EventBox from '../../components/calendar/event';
 
 const WeekCalendar = () => {
   return (
@@ -14,18 +17,23 @@ const WeekCalendar = () => {
           height={'215px'}
           locale={'ko'}
           dayHeaderFormat={{ weekday: 'short' }}
-          dayCellContent={(info) => info.date.toDateString().split(' ')[2]}
+          dayCellContent={(info) => {
+            let day = info.date.toDateString().split(' ')[2];
+            return info.isToday ? <TodayBox text={day} /> : day;
+          }}
+          eventContent={(info) => <EventBox text={info.event.title} />}
           // dayGridWeek은 'end + 1'로 표시해야한다. (적용전)
           events={[
             {
               title: 'event1',
               start: '2023-09-18',
               end: '2023-09-23',
+              color: 'pink',
             },
             {
               title: 'event2',
               start: '2023-09-20',
-              end: '2023-09-22',
+              end: '2023-09-24',
               allDay: true,
             },
             {
