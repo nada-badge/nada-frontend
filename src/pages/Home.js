3fcf6184639f781a5d535b76c92508228db9f2aa
dudgp128 <nbv19@naver.com>
2023-09-23 00:51:57 +0900

    [REFACT] 홈화면 flex-direction : column으로 설정하기 #NADA-43

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import '../components/home/style.css';
 import Banner from '../components/home/Banner';
 import BoardCardList from '../components/home/BoardCardList/BoardCardList';
 import ActivityCardList from '../components/home/ActivityCardList/ActivityCardList';
 import WeekCalendar from '../containers/calendar/WeekCalendar';
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  background-color: var(--myspec-gray-scalegray-100);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
+  width: 375px;
+  margin: 0px auto;
+
+  & > div {
+    background-color: white;
+  }
+`;
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="div-2">
-        {/* 상단 내비게이션 
+    <HomeContainer>
+      {/* 상단 내비게이션 
         <Top
           className="top-instance"
           element="https://generation-sessions.s3.amazonaws.com/34294950d7167123fb2eefcf02c0f744/img/-----1.svg"
           property1="main"
         /> */}
-        <Banner />
-        <WeekCalendar />
-        <BoardCardList />
-        <ActivityCardList />
-      </div>
-    </div>
+      <Banner />
+      <WeekCalendar />
+      <BoardCardList />
+      <ActivityCardList />
+    </HomeContainer>
   );
 };
 
