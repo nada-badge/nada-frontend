21492ee198c459ddf7f6f1ee9b0b87a9fbd03673
dudgp128 <20181048@sungshin.ac.kr>
2023-10-09 11:09:46 +0900

    [STYLE] styled-components로 재사용 가능한 폰트 스타일 객체 생성하기 #NADA-52

---
+++ src/styles/fontStyle.js
@@ -0,0 +1,40 @@
+import { css } from 'styled-components';
+
+export const caption_01 = (color) => css`
+  color: ${color || 'var(--myspec-gray-scalegray-900)'};
+  font-family: var(--caption-01-font-family);
+  font-size: var(--caption-01-font-size);
+  font-style: var(--caption-01-font-style);
+  font-weight: var(--caption-01-font-weight);
+  letter-spacing: var(--caption-01-letter-spacing);
+  line-height: var(--caption-01-line-height);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+export const caption_02 = (color) => css`
+  color: ${color || 'var(--myspec-primaryblue-1)'};
+  font-family: var(--caption-02-font-family);
+  font-size: var(--caption-02-font-size);
+  font-style: var(--caption-02-font-style);
+  font-weight: var(--caption-02-font-weight);
+  letter-spacing: var(--caption-02-letter-spacing);
+  line-height: var(--caption-02-line-height);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+export const subtitle_03 = (color) => css`
+  color: ${color || 'var(--myspec-gray-scalegray-900)'};
+  font-family: var(--subtitle-03-font-family);
+  font-size: var(--subtitle-03-font-size);
+  font-style: var(--subtitle-03-font-style);
+  font-weight: var(--subtitle-03-font-weight);
+  letter-spacing: var(--subtitle-03-letter-spacing);
+  line-height: var(--subtitle-03-line-height);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
