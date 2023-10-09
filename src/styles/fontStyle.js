afc9b207ec7fd4b7adf76fbc90e5f74680a3a3ee
dudgp128 <20181048@sungshin.ac.kr>
2023-10-09 21:12:07 +0900

    [STYLE] subtitle_01 폰트 추가하기, 재사용 가능 폰트 적용하기 #NADA-52

--- src/styles/fontStyle.js
+++ src/styles/fontStyle.js
@@ -1,7 +1,7 @@
 import { css } from 'styled-components';
 
 export const caption_01 = (color) => css`
-  color: ${color || 'var(--myspec-gray-scalegray-900)'};
+  color: ${color || '#000000'};
   font-family: var(--caption-01-font-family);
   font-size: var(--caption-01-font-size);
   font-style: var(--caption-01-font-style);
@@ -26,8 +26,21 @@ export const caption_02 = (color) => css`
   overflow: hidden;
 `;
 
+export const subtitle_01 = (color) => css`
+  color: ${color || '#000000'};
+  font-family: var(--subtitle-01-font-family);
+  font-size: var(--subtitle-01-font-size);
+  font-style: var(--subtitle-01-font-style);
+  font-weight: var(--subtitle-01-font-weight);
+  letter-spacing: var(--subtitle-01-letter-spacing);
+  line-height: var(--subtitle-01-line-height);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
 export const subtitle_03 = (color) => css`
-  color: ${color || 'var(--myspec-gray-scalegray-900)'};
+  color: ${color || '#000000'};
   font-family: var(--subtitle-03-font-family);
   font-size: var(--subtitle-03-font-size);
   font-style: var(--subtitle-03-font-style);
