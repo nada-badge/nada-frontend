30aa98f6438435b52dba15b9cfbeaad19ccec6c4
daeun lee <comdang23@gmail.com>
2023-10-10 19:59:36 +0900

    [CHORE] frame styled 간단 수정 NADA-49

--- src/community/PostList/PostItem.js
+++ src/community/PostList/PostItem.js
@@ -4,42 +4,8 @@ const PostContainer = styled.div`
   height: 90px;
   position: relative;
   width: 345px;
-
-  & > .title {
-    color: #000000;
-    font-family: var(--subtitle-03-font-family);
-    font-size: var(--subtitle-03-font-size);
-    font-style: var(--subtitle-03-font-style);
-    font-weight: var(--subtitle-03-font-weight);
-    left: 76px;
-    letter-spacing: var(--subtitle-03-letter-spacing);
-    line-height: var(--subtitle-03-line-height);
-    position: absolute;
-    top: 12px;
-    white-space: nowrap;
-    width: 269px;
-    padding: 0px 12px;
-    text-align: left;
-  }
-
-  & > .text {
-    color: #000000;
-    font-family: var(--body-01-font-family);
-    font-size: var(--body-01-font-size);
-    font-style: var(--body-01-font-style);
-    font-weight: var(--body-01-font-weight);
-    left: 76px;
-    letter-spacing: var(--body-01-letter-spacing);
-    line-height: var(--body-01-line-height);
-    position: absolute;
-    top: 36px;
-    white-space: nowrap;
-    width: 268px;
-    padding: 0px 12px;
-    text-align: left;
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
+  display: flex;
+  flex-direction: row;
 
   & > .image {
     height: 64px;
@@ -50,34 +16,76 @@ const PostContainer = styled.div`
     background-image: url("https://c.animaapp.com/hs22pPSO/img/image-3.svg");
   }
 
-  & > .frame {
-    align-items: center;
-    display: inline-flex;
-    gap: 4px;
-    left: 187px;
+  & > .innerBox {
+    display: flex;
+    flex-direction: column;
     position: absolute;
-    top: 62px;
+    width: 269px;
+    height: 64px;
+    top: 13px;
+    left: 76px;
 
-    & > .text-2 {
-      color: var(--myspec-gray-scalegray-600);
-      font-family: var(--caption-03-font-family);
-      font-size: var(--caption-03-font-size);
-      font-style: var(--caption-03-font-style);
-      font-weight: var(--caption-03-font-weight);
-      letter-spacing: var(--caption-03-letter-spacing);
-      line-height: var(--caption-03-line-height);
-      margin-top: -1px;
+    & > .title {
+      color: #000000;
+      font-family: var(--subtitle-03-font-family);
+      font-size: var(--subtitle-03-font-size);
+      font-style: var(--subtitle-03-font-style);
+      font-weight: var(--subtitle-03-font-weight);
+      letter-spacing: var(--subtitle-03-letter-spacing);
+      line-height: var(--subtitle-03-line-height);
       position: relative;
-      text-align: right;
       white-space: nowrap;
-      width: fit-content;
+      width: 269px;
+      text-align: left;
     }
-    & > .ellipse {
-      background-color: var(--myspec-gray-scalegray-600);
-      border-radius: 1px;
-      height: 2px;
+
+    & > .text {
+      color: #000000;
+      font-family: var(--body-01-font-family);
+      font-size: var(--body-01-font-size);
+      font-style: var(--body-01-font-style);
+      font-weight: var(--body-01-font-weight);
+
+      letter-spacing: var(--body-01-letter-spacing);
+      line-height: var(--body-01-line-height);
       position: relative;
-      width: 2px;
+      top: 5px;
+      white-space: nowrap;
+      width: 268px;
+      text-align: left;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+
+    & > .frame {
+      align-items: center;
+      position: relative;
+      margin-left: auto;
+      display: flex;
+      gap: 4px;
+      top: 12px;
+
+      & > .text-2 {
+        color: var(--myspec-gray-scalegray-600);
+        font-family: var(--caption-03-font-family);
+        font-size: var(--caption-03-font-size);
+        font-style: var(--caption-03-font-style);
+        font-weight: var(--caption-03-font-weight);
+        letter-spacing: var(--caption-03-letter-spacing);
+        line-height: var(--caption-03-line-height);
+        margin-top: -1px;
+        position: relative;
+        text-align: right;
+        white-space: nowrap;
+        width: fit-content;
+      }
+      & > .ellipse {
+        background-color: var(--myspec-gray-scalegray-600);
+        border-radius: 1px;
+        height: 2px;
+        position: relative;
+        width: 2px;
+      }
     }
   }
 `;
@@ -88,16 +96,18 @@ const PostItem = ({ card }) => {
   return (
     <PostContainer>
       <div className="image" />
-      <div className="title">{title}</div>
-      <div className="text">{text}</div>
-      <div className="frame">
-        <div className="text-2">{area}</div>
-        <div className="ellipse" />
-        <div className="text-2">{field}</div>
-        <div className="ellipse" />
-        <div className="text-2">{category}</div>
-        <div className="ellipse" />
-        <div className="text-2">{postAt}</div>
+      <div className="innerBox">
+        <div className="title">{title}</div>
+        <div className="text">{text}</div>
+        <div className="frame">
+          <div className="text-2">{area}</div>
+          <div className="ellipse" />
+          <div className="text-2">{field}</div>
+          <div className="ellipse" />
+          <div className="text-2">{category}</div>
+          <div className="ellipse" />
+          <div className="text-2">{postAt}</div>
+        </div>
       </div>
     </PostContainer>
   );
