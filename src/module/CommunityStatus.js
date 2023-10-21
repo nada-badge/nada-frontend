f665f764b4912dd0b0004c42fbdf629302397095
daeun lee <comdang23@gmail.com>
2023-10-21 14:31:52 +0900

    [FEAT] 커뮤니티 메인 페이지 필터 구현 커뮤니티 리덕스의 subButtonSelect 의 값을 통해 바로 검색하면 됨

--- src/module/CommunityStatus.js
+++ src/module/CommunityStatus.js
@@ -2,9 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
 
 const initialState = {
-  community: {
-    filterActive: "",
-    categoryActive: "",
+  buttonSelect: {
+    maincategory: "모집",
+    filter: 0,
+  },
+  subButtonSelect: {
+    area: ["전국"],
+    field: ["전체"],
+    category: ["전체"],
   },
 };
 
@@ -12,17 +17,33 @@ const communitySlice = createSlice({
   name: "community",
   initialState,
   reducers: {
-    changeField: (state, { payload: { form, key, value } }) => {
+    setField: (state, { payload: { form, key, value } }) => {
       state[form][key] = value;
     },
-    initializeForm: (state, { payload: form }) => {
-      state[form] = initialState[form];
+    addField: (state, { payload: { form, key, value } }) => {
+      state[form][key].push(value);
+    },
+    deleteField: (state, { payload: { form, key, value } }) => {
+      const temp = state[form][key].filter((element) => element !== value);
+      state[form][key] = temp;
+    },
+    initializeAll: (state) => {
+      return initialState;
+    },
+    initializeForm: (state, { payload: { form, key } }) => {
+      state[form][key] = initialState[form][key];
     },
   },
 });
 
 export default communitySlice;
-export const { changeField, initializeForm } = communitySlice.actions;
+export const {
+  setField,
+  initializeForm,
+  addField,
+  deleteField,
+  initializeAll,
+} = communitySlice.actions;
 
 const communitySelects = (type, field) => (rootState) => {
   return rootState.community[type][field] || initialState[type][field];
