fe14b1c757f7ab4e9c5e273bcb5b7d0b86ce8e21
dudgp128 <20181048@sungshin.ac.kr>
2023-10-25 21:22:22 +0900

    [FEAT] 로그인 User Redux로 상태 관리하기
    email, token 전역상태로 저장하기

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import schedule from './schedule';
+import user from './user';
 
 const rootReducer = combineReducers({
   auth,
-  schedule
+  schedule,
+  user,
 });
 
 export default rootReducer;
