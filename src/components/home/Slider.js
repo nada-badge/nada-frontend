94c5ea1b80ddb7fe8cc3dd22dce579c78f45f377
dudgp128 <20181048@sungshin.ac.kr>
2023-10-26 22:55:45 +0900

    [FEAT] Banner Slide autoPlay, paging 효과 적용하기 #NADA-43

--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -6,19 +6,52 @@ import Banner4 from '../../images/Banner/Banner4.png';
 import Banner5 from '../../images/Banner/Banner5.png';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import styled from 'styled-components';
+import { caption_02 } from '../../styles/fontStyle';
+
+const SliderWrapper = styled.div`
+  & > div > .custom_paging {
+    position: absolute;
+    top: 84px;
+    left: 326px;
+    margin: 0;
+    padding: 0;
+
+    & > li {
+      display: none;
+      background-color: #00000099;
+      border-radius: 12px;
+      justify-content: center;
+      padding: 2px 8px;
+      width: fit-content;
+
+      & > div {
+        ${caption_02('#ffffff')}
+      }
+    }
+    & > .slick-active {
+      display: block;
+    }
+  }
+`;
 
 const BannerSlider = () => {
   const settings = {
-    dots: false,
+    dots: true,
     arrows: false,
+    autoplay: true,
     infinite: true,
-    speed: 200,
+    speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
+    dotsClass: 'custom_paging',
+    customPaging: function (slider) {
+      return <div>{slider + 1}/5</div>;
+    },
   };
 
   return (
-    <div>
+    <SliderWrapper>
       <Slider {...settings}>
         <div>
           <img alt="banner" src={Banner1} />
@@ -36,7 +69,7 @@ const BannerSlider = () => {
           <img alt="banner" src={Banner5} />
         </div>
       </Slider>
-    </div>
+    </SliderWrapper>
   );
 };
 
