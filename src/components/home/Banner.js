594bfb383bfc3210dd7d670fe973f327666aaca4
dudgp128 <nbv19@naver.com>
2023-10-26 22:55:45 +0900

    [FEAT] Banner Slide autoPlay, paging 효과 적용하기 #NADA-43

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -8,42 +8,12 @@ const BannerContainer = styled.div`
   position: relative;
   /* top: 89px;  상단, 하단 네비게이션 적용후 수정 예정*/
   width: 375px;
-
-  & > .IndicatorWrapper {
-    align-items: center;
-    background-color: #00000099;
-    border-radius: 12px;
-    display: inline-flex;
-    flex-direction: column;
-    gap: 10px;
-    justify-content: center;
-    left: 326px;
-    padding: 2px 8px;
-    position: absolute;
-    top: 84px;
-
-    & > .text {
-      color: #ffffff;
-      font-family: var(--caption-02-font-family);
-      font-size: var(--caption-02-font-size);
-      font-style: var(--caption-02-font-style);
-      font-weight: var(--caption-02-font-weight);
-      letter-spacing: var(--caption-02-letter-spacing);
-      line-height: var(--caption-02-line-height);
-      margin-top: -1px;
-      position: relative;
-      white-space: nowrap;
-      width: fit-content;
-    }
-  }
 `;
+
 const Banner = () => {
   return (
     <BannerContainer>
       <BannerSlider />
-      <div className="IndicatorWrapper">
-        <div className="text">1/5</div>
-      </div>
     </BannerContainer>
   );
 };
