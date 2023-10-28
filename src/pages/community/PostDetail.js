7d4fd0dc1cd04b3cd36200d6a7eb1dee76b33e67
daeun lee <comdang23@gmail.com>
2023-10-28 13:08:53 +0900

    [FEAT] 커뮤니티 디테일 하단바 구현 #NADA-49

--- src/pages/community/PostDetail.js
+++ src/pages/community/PostDetail.js
@@ -1,6 +1,7 @@
 import TagButtons from "../../components/TagButton";
 import { ContentsList } from "../../styles/PostDetailStyle";
 import { useSelector } from "react-redux";
+import { BottomBar } from "../../container/community/bottomBar";
 import Dateformat from "../../components/Dateformat";
 
 const PostDetail = () => {
@@ -21,26 +22,29 @@ const PostDetail = () => {
   console.log("PostDetail : ", area);
 
   return (
-    <ContentsList>
-      <div className="title">
-        <div className="p">{title}</div>
-      </div>
-      <div className="profile">
-        <div className="image" />
-        <div className="text">
-          <div className="nickname">{userName}</div>
-          <div className="date">{Dateformat(updatedAt)}</div>
+    <div>
+      <ContentsList>
+        <div className="title">
+          <div className="p">{title}</div>
         </div>
-      </div>
-      <div className="tag">
-        <TagButtons />
-      </div>
-      <div className="content">
-        <div className="text">{content} </div>
-        <div className="image-2" />
-        <div className="image-2" />
-      </div>
-    </ContentsList>
+        <div className="profile">
+          <div className="image" />
+          <div className="text">
+            <div className="nickname">{userName}</div>
+            <div className="date">{Dateformat(updatedAt)}</div>
+          </div>
+        </div>
+        <div className="tag">
+          <TagButtons />
+        </div>
+        <div className="content">
+          <div className="text">{content} </div>
+          <div className="image-2" />
+          <div className="image-2" />
+        </div>
+      </ContentsList>
+      <BottomBar />
+    </div>
   );
 };
 
