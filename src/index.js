af4d06f3ca5f7f6f5d8b0995d96ca56b029ba744
daeun lee <comdang23@gmail.com>
2023-11-02 18:16:29 +0900

    [CHORE] BrowserRouter를 app에서 index로 위치 변경#NADA-49

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "./styleguide.css";
@@ -26,7 +27,9 @@ export const store = configureStore({
 
 root.render(
   <Provider store={store}>
-    <App />
-    <ModalContainer />
+    <BrowserRouter>
+      <App />
+      <ModalContainer />
+    </BrowserRouter>
   </Provider>
 );
