258fc9909bf158ae31371dae73f795328c7b0c98
daeun lee <comdang23@gmail.com>
2023-11-02 18:16:29 +0900

    [CHORE] BrowserRouter를 app에서 index로 위치 변경#NADA-49

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Community from "./pages/community/Community";
 import PostWrite from "./pages/community/PostWrite";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -11,13 +11,11 @@ function App() {
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/community" element={<Community />} />
-            <Route path="/community/PostWrite" element={<PostWrite />} />
-            <Route path="/community/PostDetail/:_id" element={<PostDetail />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/community" element={<Community />} />
+          <Route path="/community/PostWrite" element={<PostWrite />} />
+          <Route path="/community/PostDetail/:_id" element={<PostDetail />} />
+        </Routes>
       </QueryClientProvider>
     </div>
   );
