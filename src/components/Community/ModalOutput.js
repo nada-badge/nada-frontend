9cd137e75349815eb4267ace641069b881cdd549
daeun lee <comdang23@gmail.com>
2023-11-05 20:52:27 +0900

    [REFACT] 신고/삭제 질문  모달 컴포넌트화 #NADA-49

---
+++ src/components/Community/ModalOutput.js
@@ -0,0 +1,28 @@
+import useModal from "../Modal/Community/useModal";
+import ModalButtonDiv from "./ModalButtonDiv";
+import { Layout } from "../../styles/Community/BarSecModalStyle";
+
+export const ModalOutPut = ({ title, content, act, actText }) => {
+  const { closeModal } = useModal();
+  const Close = () => {
+    closeModal();
+  };
+
+  return (
+    <Layout>
+      <div className="title">{title}</div>
+      <div className="border" />
+      <p className="content">{content}</p>
+      <div className="border-2" />
+      {ModalButtonDiv({
+        cancelText: "취소",
+        cancel: Close,
+        actText: actText,
+        act: act,
+        isRed: true,
+      })}
+    </Layout>
+  );
+};
+
+export default ModalOutPut;
--- src/components/Modal/Community/DeleteModal.js
+++ src/components/Modal/Community/DeleteModal.js
@@ -5,8 +5,7 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import client from "../../../lib/api/client";
 import { PostDetailSelector } from "../../../module/Community/postDetail";
-import { Layout } from "../../../styles/Community/BarSecModalStyle";
-import ModalButtonDiv from "../../community/ModalButtonDiv";
+import ModalOutPut from "../../community/ModalOutput";
 
 function DeleteModal() {
   const navigate = useNavigate();
@@ -22,25 +21,14 @@ function DeleteModal() {
     openModal({ type: "NoticeModal" });
   }, [id, navigate]);
 
-  const Close = () => {
-    closeModal();
-  };
-
   return (
     <Modal onClose={closeModal}>
-      <Layout>
-        <div className="title">글을 삭제할까요?</div>
-        <div className="border" />
-        <p className="content">한 번 삭제한 글은 복구할 수 없어요</p>
-        <div className="border-2" />
-        {ModalButtonDiv({
-          cancelText: "취소",
-          cancel: Close,
-          actText: "삭제",
-          act: Delete,
-          isRed: true,
-        })}
-      </Layout>
+      {ModalOutPut({
+        title: "글을 삭제할까요?",
+        content: "한 번 삭제한 글은 복구할 수 없어요",
+        act: Delete,
+        actText: "삭제",
+      })}
     </Modal>
   );
 }
