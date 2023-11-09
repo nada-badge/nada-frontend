90d1c67f28e2cbf717923dc824164d8a63a42618
daeun lee <comdang23@gmail.com>
2023-11-09 17:55:36 +0900

    [FEAT] 북마크 기능 구현 #NADA-49

--- src/container/community/postDetail/bottomBar.js
+++ src/container/community/postDetail/bottomBar.js
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import {
   Bar,
   MenuImg,
   BookmarkImg,
 } from "../../../styles/Community/CommunityBarStyle";
 import useModal from "../../../components/Modal/Community/useModal";
+import classNames from "classnames";
 
 export const BottomBar = () => {
   const { openModal } = useModal();
-  const onClick = () => {
-    openModal({ type: "MenuModal" });
-    console.log("메뉴가 클릭되었습니다.");
+  const [isActive, setIsActive] = useState();
+
+  const openMenu = () => {
+    openModal({ type: "YourMenuModal" });
+  };
+
+  const bookMark = () => {
+    setIsActive(!isActive);
+    //북마크 여부 정보를 넘기는 코드 작성 예정
   };
 
   return (
     <Bar>
-      <MenuImg onClick={onClick} />
+      <MenuImg onClick={openMenu} />
+      <BookmarkImg onClick={bookMark} className={classNames({ isActive })} />
       <div className="div">
         <div className="comment">
           <div className="group">
@@ -25,7 +34,6 @@ export const BottomBar = () => {
         </div>
         <div className="text">23</div>
       </div>
-      <BookmarkImg />
     </Bar>
   );
 };
