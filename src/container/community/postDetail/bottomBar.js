c9e8abd4ab3cba93aa834d31918d4c9794aaaeba
daeun lee <comdang23@gmail.com>
2023-11-11 12:58:14 +0900

    [CHORE] 댓글클릭 시 commentPage로 이동#NADA-49

--- src/container/community/postDetail/bottomBar.js
+++ src/container/community/postDetail/bottomBar.js
@@ -6,9 +6,14 @@ import {
 } from "../../../styles/Community/CommunityBarStyle";
 import useModal from "../../../components/Modal/Community/useModal";
 import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { PostDetailSelector } from "../../../module/Community/postDetail";
 
 export const BottomBar = () => {
   const { openModal } = useModal();
+  const navigate = useNavigate();
+  const id = useSelector(PostDetailSelector("_id"));
   const [isActive, setIsActive] = useState();
 
   const openMenu = () => {
@@ -20,13 +25,17 @@ export const BottomBar = () => {
     //북마크 여부 정보를 넘기는 코드 작성 예정
   };
 
+  const openComment = () => {
+    navigate(`/community/Comment/${id}`);
+  };
+
   return (
     <Bar>
       <MenuImg onClick={openMenu} />
       <BookmarkImg onClick={bookMark} className={classNames({ isActive })} />
       <div className="div">
         <div className="comment">
-          <div className="group">
+          <div className="group" onClick={openComment}>
             <div className="ellipse" />
             <div className="ellipse-2" />
             <div className="ellipse-3" />
