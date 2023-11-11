ff4bbf9206e1084795e873709071620160a0ac3c
daeun lee <comdang23@gmail.com>
2023-11-11 14:55:28 +0900

    [FEAT] 답글 post 및 화면과 API 연동 #NADA-49

---
+++ src/module/queries/replyQuery.js
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import client from "../../lib/api/client";
+import { useSelector } from "react-redux";
+import { PostDetailSelector } from "../Community/postDetail";
+
+const useReply = () => {
+  const postId = useSelector(PostDetailSelector("PostDetail", "_id"));
+  const commentId = useSelector(PostDetailSelector("Comment", "_id"));
+  const userEmail = "20230904@nate.com";
+  const userName = "오늘닉네임";
+
+  const mutation = useMutation({
+    mutationFn: ({ inputValue }) =>
+      client.post(`/community/reply/${postId}/${commentId}`, {
+        userEmail,
+        userName,
+        content: inputValue,
+      }),
+    onSuccess: (data) => {
+      console.log("답글이 달렸습니다.", data);
+    },
+    onError: (error) => {
+      console.log("답글 게시에 실패하였습니다.", error);
+    },
+  });
+
+  return mutation;
+};
+
+export default useReply;
