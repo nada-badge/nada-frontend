dc7e2ffdad28d8d12c55e995a39948d9f027f6db
dudgp128 <20181048@sungshin.ac.kr>
2023-11-22 12:23:52 +0900

    [REFACT] 클릭한 날짜 기준으로 이벤트 필터링하기 #NADA-50

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -104,14 +104,22 @@ const CalendarPage = () => {
 
   // 주어진 날짜를 기준으로 이벤트를 필터링
   const filterEvent = useCallback(
-    (dateStr) => {
-      const baseDay = Number(dateStr.split('-')[2]);
+    (baseDateStr) => {
+      const baseDate = new Date(baseDateStr);
+      const formatDate = (date) => date.toISOString().slice(0, 10);
 
-      return events.filter(({ start, end }) => {
-        const startDay = Number(start.split('-')[2]);
-        const endDay = end ? Number(end.split('-')[2]) : startDay;
-        return startDay <= baseDay && baseDay <= endDay;
-      });
+      return events
+        .map(({ title, start, end }) => ({
+          title,
+          start: new Date(start),
+          end: new Date(end),
+        }))
+        .filter(({ start, end }) => start <= baseDate && baseDate <= end)
+        .map(({ title, start, end }) => ({
+          title,
+          start: formatDate(start),
+          end: formatDate(end),
+        }));
     },
     [events],
   );
