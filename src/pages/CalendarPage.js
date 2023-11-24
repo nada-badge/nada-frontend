2dd8d3da9b087d7b5af41ddd0dc1770ce27aa796
dudgp128 <nbv19@naver.com>
2023-11-24 16:42:00 +0900

    [FEAT] 이벤트에 색상 입히기 #NADA-50

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -12,6 +12,7 @@ import { changeField } from '../modules/calendar/calendar';
 import useEventsQuery from '../modules/queries/EventQuery';
 import { setBarStatus } from '../modules/bar';
 import { filter } from '../modules/calendar/filterEvent';
+import { generateRGBAArray } from './color';
 
 const Div = styled.div`
   background-color: #ffffff;
@@ -65,10 +66,17 @@ const CalendarPage = () => {
       return;
     }
 
-    const MonthEvent = (data.activities || []).map((e) => ({
+    // 월(month) 데이터에 따라 색상 불러오기
+    const colorList = generateRGBAArray(
+      today.getMonth(),
+      data.activities.length,
+    );
+
+    const MonthEvent = (data.activities || []).map((e, idx) => ({
       title: e.activityName,
       start: e.startedAt.slice(0, 10),
       end: e.endedAt.slice(0, 10),
+      color: colorList[idx],
     }));
 
     setEvents(MonthEvent);
