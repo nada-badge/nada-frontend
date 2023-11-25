20dfdcd14640feb1600f0ad436418b8ad6c6506d
daeun lee <comdang23@gmail.com>
2023-11-25 20:16:35 +0900

    [CHORE] 간단한 코드 수정 #NADA-49

--- src/containers/community/postDetail/BottomBar.js
+++ src/containers/community/postDetail/BottomBar.js
@@ -1,23 +1,20 @@
 /** BottomBar 세부 게시글 화면에서 보여지는 하단바 */
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Bar, MenuImg } from '../../../styles/Community/CommunityBarStyle';
 import useModal from '../../../components/Modal/Community/useModal';
-import classNames from 'classnames';
 import { PostDetailSelector } from '../../../modules/Community/postDetail';
 
 export const BottomBar = () => {
   const { openModal } = useModal();
   const navigate = useNavigate();
   const id = useSelector(PostDetailSelector('PostDetail', '_id'));
-  const [isActive, setIsActive] = useState();
 
   const openMenu = () => {
     openModal({
       type: 'MenuModal',
       contentType: '글',
-      content: ['Share', 'Delete'],
+      content: ['Share', 'Delete', 'Report', 'Update'],
       position: 'post',
     });
   };
