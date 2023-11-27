13097919c59a1dbe45322f9b0702f48fc501eac0
dudgp128 <nbv19@naver.com>
2023-11-27 18:51:50 +0900

    [FEAT] 배지 아이템 컴포넌트 만들기 #NADA-60

---
+++ src/components/cardList/BadgeItem.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { body_01, caption_02, subtitle_01 } from '../../styles/fontStyle';
+const BadgeContainer = styled.div`
+  display: flex;
+  width: 120px;
+  height: 120px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > .imgWrapper {
+    display: flex;
+    width: 120px;
+    height: 120px;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & > .textWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    & > .badgeType {
+      ${caption_02('var(--myspec-gray-scale-gray-600, #888)')}
+    }
+
+    & > .title {
+      ${subtitle_01('var(--myspec-gray-scale-gray-900, #1A1A1A)')}
+    }
+
+    & > .roleWrapper {
+      display: flex;
+      align-items: flex-start;
+      gap: 4px;
+      ${body_01('var(--myspec-gray-scale-gray-600, #888)')}
+    }
+  }
+`;
+
+export const BadgeItem = ({ card }) => {
+  const { id, year, img_src, badgeType, title, team, role } = card;
+  return (
+    <BadgeContainer>
+      <div className="imgWrapper" src={img_src} />
+      <div className="textWrapper">
+        <div className="badgeType"> {badgeType} </div>
+        <div className="title"> {title}</div>
+        <div className="roleWrapper">
+          <div className="team">{team}</div>
+          <div className="role">{role}</div>
+        </div>
+      </div>
+    </BadgeContainer>
+  );
+};
