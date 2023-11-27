9084131b30d36cf5223d1ae5b0bd2bda1ba9716f
dudgp128 <nbv19@naver.com>
2023-11-28 01:34:20 +0900

    [FEAT] 캘린더 일정 모달 이벤트에 따라 색상 입히기 #NADA-50

--- src/modules/calendar/filterEvent.js
+++ src/modules/calendar/filterEvent.js
@@ -3,15 +3,17 @@ export const filter = (baseDateStr, events) => {
   const formatDate = (date) => date.toISOString().slice(0, 10);
 
   return events
-    .map(({ title, start, end }) => ({
+    .map(({ title, start, end, color }) => ({
       title,
       start: new Date(start),
       end: new Date(end),
+      color: color,
     }))
     .filter(({ start, end }) => start <= baseDate && baseDate <= end)
-    .map(({ title, start, end }) => ({
+    .map(({ title, start, end, color }) => ({
       title,
       start: formatDate(start),
       end: formatDate(end),
+      color: color,
     }));
 };
