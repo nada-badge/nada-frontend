3188d367bbe166abf579ac8bc008156b5976c10c
daeun lee <comdang23@gmail.com>
2023-11-28 13:49:11 +0900

    [REFACT] 태그버튼 파일 분리 #NADA-49"

--- src/components/Community/postDetail/TagButtons.js
+++ src/components/Community/postDetail/TagButtons.js
@@ -1,20 +1,12 @@
 /** TagButton region, field, category 정보를 태그 형식으로 출력하는 컴포넌트 */
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
-import { body_01 } from '../../../styles/fontStyle';
+import TagButton from '../../common/TagButton';
 
 const TagButtons = () => {
   const PostDetail = useSelector(({ postdetail }) => postdetail.PostDetail);
   const { region, field, category } = PostDetail;
   const combinedArray = [...region, ...field, category];
-
-  const TagButton = ({ tag }) => {
-    return (
-      <Button>
-        <TextWarpper>{tag}</TextWarpper>
-      </Button>
-    );
-  };
 
   return (
     <TagList>
@@ -32,23 +24,4 @@ export const TagList = styled.div`
   flex-direction: row;
   gap: 8px;
   flex-wrap: wrap;
-`;
-
-export const Button = styled.div`
-  align-items: center;
-  border-radius: 16px;
-  display: inline-flex;
-  overflow: hidden;
-  position: relative;
-  background-color: var(--myspec-gray-scalegray-100);
-  gap: 8px;
-  padding: 4px 12px;
-`;
-
-export const TextWarpper = styled.div`
-  ${body_01('var(--myspec-gray-scalegray-600)')}
-  margin-top: -1px;
-  position: relative;
-  white-space: nowrap;
-  width: fit-content;
 `;
