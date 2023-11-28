494f840549174b91e7ca1b97c38c6988c20547ad
dudgp128 <20181048@sungshin.ac.kr>
2023-11-28 21:50:26 +0900

    [FEAT] 배지 상세 페이지 BadgeItem 재사용해서 컴포넌트 적용하기 #NADA-60

--- src/components/cardList/BadgeItem.js
+++ src/components/cardList/BadgeItem.js
@@ -2,16 +2,18 @@ import styled from 'styled-components';
 import { body_01, caption_02, subtitle_01 } from '../../styles/fontStyle';
 
 const BadgeContainer = styled.div`
+  
   display: flex;
-  width: calc(120px - 12px);
-  height: 120px;
+  width: ${(props) => props.$layout_style?.width || 'calc(132px - 12px)'};
+  height: ${(props) => props.$layout_style?.height || '195px'};
   flex-direction: column;
   justify-content: center;
+  gap: ${(props) => props.$layout_style?.gap || '12px'};
 
   & > .imgWrapper {
     display: flex;
     width: 100%;
-    height: 120px;
+    aspect-ratio: 1 / 1;
     justify-content: center;
     align-items: center;
   }
@@ -20,32 +22,33 @@ const BadgeContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    text-align: left;
+    text-align: ${(props) => props.$layout_style?.textAlign || 'left'};
 
     & > .badgeType {
       ${caption_02('var(--myspec-gray-scale-gray-600, #888)')}
     }
 
     & > .title {
+      padding-top: ${(props) => props.$layout_style?.paddingTop || ''};
       width: inherit;
       ${subtitle_01('var(--myspec-gray-scale-gray-900, #1A1A1A)')}
     }
 
     & > .roleWrapper {
       width: inherit;
-      display: flex;
-      align-items: flex-start;
+      display: inline-flex;
+      justify-content: ${(props) =>
+        props.$layout_style?.justifyContent || 'flex-start'};
       gap: 4px;
-      ${body_01('var(--myspec-gray-scale-gray-600, #888)')}
+      ${body_01('var(--myspec-gray-scale-gray-600, #888)')};
     }
   }
 `;
 
-export const BadgeItem = ({ card }) => {
+export const BadgeItem = ({ card, $layout_style }) => {
   const { id, year, img_src, badgeType, title, team, role } = card;
   return (
-    <BadgeContainer>
+    <BadgeContainer $layout_style={$layout_style}>
       <div className="imgWrapper" src={img_src} />
       <div className="textWrapper">
         <div className="badgeType"> {badgeType} </div>
--- src/pages/badge/DetailBadgePage.js
+++ src/pages/badge/DetailBadgePage.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setBarStatus } from '../../modules/bar';
+import { BadgeItem } from '../../components/cardList/BadgeItem';
 
 const DetailBadgePage = () => {
   const dispatch = useDispatch();
@@ -21,11 +22,38 @@ const DetailBadgePage = () => {
     flexDirection: 'column',
     gap: '12px',
     margin: '0 auto',
+    background: 'var(--myspec-gray-sclegray-border)',
+  };
+
+  const BadgeWrapper = {
+    background: 'white',
+    padding: '24px 112px',
+  };
+
+  const badge_info = {
+    id: 1,
+    year: 2023,
+    img_src: '',
+    badgeType: '교내 동아리',
+    title: 'TEAM NADA 2기',
+    team: '디자인팀',
+    role: '팀원',
+  };
+
+  const layout_style = {
+    width: '150px',
+    height: 'fit-content', // 이미지 + 마지막 text 위치 + text height
+    gap: '14px',
+    textAlign: 'center',
+    justifyContent: 'center',
+    paddingTop: '0.3em',
   };
 
   return (
     <div style={pageContainer}>
-      <div>{'뱃지 썸네일'}</div>
+      <div style={BadgeWrapper}>
+        <BadgeItem card={badge_info} $layout_style={layout_style} />
+      </div>
       <div>{'뱃지 설명'}</div>
       <div>{'활동 내역'}</div>
       <div>{'활동 사진'}</div>
