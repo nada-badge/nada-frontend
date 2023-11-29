5d01ba36d0d122ca83acc5ab69678ddba7a0b90a
daeun lee <comdang23@gmail.com>
2023-11-29 20:17:23 +0900

    [CHORE] 간단한 코드 수정 #NADA-49

--- src/components/Community/SelectButton.js
+++ src/components/Community/SelectButton.js
@@ -1,17 +1,45 @@
 /** SelectButton  region, field, category 모달 내 개별요소에 대한 버튼 */
-import SelectButtonBase from '../common/SelectButtonBase';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { filterSelector, addField, deleteField } from '../../modules/filter';
 import { selectAllConfig } from '../common/AreaFieldCategoryData';
-import { filterSelector } from '../../modules/filter';
+import { AttributeInfoButton } from '../common/AttributeInfoButton';
 
 export const SelectButton = ({ text }) => {
+  const dispatch = useDispatch();
+  const content = useSelector(filterSelector('buttonSelect', 'filter'));
+  const state = useSelector(filterSelector('subButtonSelect', content));
+
+  const [isActive, setIsActive] = useState(state.includes(text));
+  const form = 'subButtonSelect';
+  const slectAll = selectAllConfig(content);
+
+  const onClicks = () => {
+    const deactivateButton = () => {
+      dispatch(deleteField({ form, key: content, value: text }));
+      if (state.length === 1) {
+        dispatch(addField({ form, key: content, value: slectAll }));
+      }
+    };
+
+    const activateButton = () => {
+      dispatch(addField({ form, key: content, value: text }));
+      dispatch(deleteField({ form, key: content, value: slectAll }));
+    };
+
+    isActive ? deactivateButton() : activateButton();
+  };
+
+  useEffect(() => {
+    setIsActive(state.includes(text));
+  }, [state, text]);
+
   return (
-    <SelectButtonBase
+    <AttributeInfoButton
       text={text}
-      formKey="subButtonSelect"
-      contentType={filterSelector('buttonSelect', 'filter')}
-      selectAllConfig={selectAllConfig(
-        filterSelector('buttonSelect', 'filter'),
-      )}
+      onClicks={onClicks}
+      isActive={isActive}
+      isImg={true}
     />
   );
 };
--- src/styles/Community/CommunityStyle.js
+++ src/styles/Community/CommunityStyle.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { subtitle_02 } from '../fontStyle';
 
 export const CommunityPage = styled.div`
   align-items: flex-start;
@@ -10,40 +9,6 @@ export const CommunityPage = styled.div`
   text-align: left;
   gap: 12px 0px;
   background-color: var(--myspec-gray-scalegray-100);
-`;
-
-export const Search = styled.div`
-  background-color: var(--myspec-gray-scalewhite);
-  height: 64px;
-  position: relative;
-  width: 375px;
-
-  & > .Input {
-    background-color: var(--myspec-gray-scalegray-100);
-    border-radius: 10px;
-    height: 40px;
-    left: 15px;
-    overflow: hidden;
-    position: relative;
-    top: 12px;
-    width: 345px;
-
-    & > .Text {
-      ${subtitle_02('var(--myspec-gray-scalegray-600)')}
-      height: 24px;
-      left: 12px;
-      position: absolute;
-      top: 7px;
-    }
-
-    & > .SearchImg {
-      height: 24px;
-      left: 309px;
-      position: absolute;
-      top: 8px;
-      width: 24px;
-    }
-  }
 `;
 
 export const Border = styled.div`
