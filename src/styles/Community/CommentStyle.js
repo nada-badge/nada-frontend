e82455e3854587fca7774a4b36a926aa734531b6
daeun lee <comdang23@gmail.com>
2023-11-29 20:14:10 +0900

    [REFACT] comment와 reply중 중복 코드 컴포넌트화 #NADA-49

--- src/styles/Community/CommentStyle.js
+++ src/styles/Community/CommentStyle.js
@@ -1,6 +1,76 @@
 import styled from 'styled-components';
 import { caption_02, caption_03, body_01 } from '../fontStyle';
 
+const commonProfileStyle = `
+  align-items: center;
+  display: inline-flex;
+  flex: 0 0 auto;
+  gap: 12px;
+  position: relative;
+
+  & > .image {
+    background-color: var(--myspec-gray-scalegray-300);
+    border-radius: 30px;
+    height: 32px;
+    position: relative;
+    width: 32px;
+  }
+
+  & > .name-date {
+    position: relative;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    flex: 0 0 auto;
+
+    & > .name {
+      ${caption_02('var(--myspec-gray-scalegray-900)')}
+      margin-top: -1px;
+      position: relative;
+      width: 136px;
+    }
+
+    & > .date {
+      ${caption_03('var(--myspec-gray-scalegray-900)')}
+      position: relative;
+      width: 136px;
+    }
+  }
+`;
+
+const commonMenuStyle = `
+  align-items: flex-start;
+  display: inline-flex;
+  flex: 0 0 auto;
+  gap: 10px;
+  padding: 8px;
+  position: relative;
+
+  & > .meatball {
+    display: flex;
+    flex-direction: column;
+    height: 16px;
+    position: relative;
+    width: 4px;
+    gap: 2px;
+
+    & > .ellipse {
+      background-color: var(--myspec-gray-scalegray-600);
+      border-radius: 2px;
+      height: 4px;
+      position: relative;
+      width: 4px;
+    }
+  }
+`;
+
+const commonLikesReplyStyle = `
+  align-items: flex-end;
+  display: inline-flex;
+  flex: 0 0 auto;
+  position: relative;
+`;
+
 export const CommentList = styled.div`
   align-items: flex-start;
   display: inline-flex;
@@ -16,75 +86,16 @@ export const Comment = styled.div`
   flex: 0 0 auto;
   align-items: flex-start;
   background-color: var(--myspec-gray-scalewhite);
-  border-bottom-style: solid;
-  border-bottom-width: 1px;
-  border-color: var(--myspec-gray-scalegray-200);
+  border-bottom: 1px solid var(--myspec-gray-scalegray-200);
   gap: 8px;
   padding: 16px 15px;
 
   & > .profile {
-    align-items: flex-start;
-    display: inline-flex;
-    flex: 0 0 auto;
+    ${commonProfileStyle}
     gap: 145px;
-    position: relative;
-    & > .image-name-date {
-      align-items: center;
-      display: inline-flex;
-      flex: 0 0 auto;
-      gap: 12px;
-      justify-content: center;
-      position: relative;
-      & > .image {
-        background-color: var(--myspec-gray-scalegray-300);
-        border-radius: 30px;
-        height: 32px;
-        position: relative;
-        width: 32px;
-      }
-      & > .name-date {
-        position: relative;
-        display: inline-flex;
-        flex-direction: column;
-        align-items: flex-start;
-        flex: 0 0 auto;
 
-        & > .name {
-          ${caption_02('var(--myspec-gray-scalegray-900)')}
-          margin-top: -1px;
-          position: relative;
-          width: 136px;
-        }
-        & > .date {
-          ${caption_03('var(--myspec-gray-scalegray-900)')}
-          position: relative;
-          width: 136px;
-        }
-      }
-    }
     & > .menu {
-      align-items: flex-start;
-      display: inline-flex;
-      flex: 0 0 auto;
-      gap: 10px;
-      padding: 8px;
-      position: relative;
-      & > .meatball {
-        display: flex;
-        flex-direction: column;
-        height: 16px;
-        position: relative;
-        width: 4px;
-        gap: 2px;
-
-        & > .ellipse {
-          background-color: var(--myspec-gray-scalegray-600);
-          border-radius: 2px;
-          height: 4px;
-          position: relative;
-          width: 4px;
-        }
-      }
+      ${commonMenuStyle}
     }
   }
 
@@ -95,41 +106,32 @@ export const Comment = styled.div`
   }
 
   & > .likes-reply {
-    align-items: flex-end;
-    display: inline-flex;
-    flex: 0 0 auto;
+    ${commonLikesReplyStyle}
     gap: 192px;
-    position: relative;
+
     & > .likes {
-      align-items: center;
-      display: inline-flex;
-      flex: 0 0 auto;
+      ${commonLikesReplyStyle}
       gap: 4px;
-      position: relative;
+
       & > .like {
-        align-items: center;
-        display: inline-flex;
-        flex: 0 0 auto;
+        ${commonLikesReplyStyle}
         gap: 6px;
-        position: relative;
+
         & > .text {
           ${body_01('var(--myspec-gray-scalegray-900)')}
           margin-top: -1px;
-          position: relative;
           width: 56px;
         }
       }
     }
+
     & > .reply {
-      align-items: flex-start;
-      display: inline-flex;
-      flex: 0 0 auto;
+      ${commonLikesReplyStyle}
       gap: 8px;
-      position: relative;
+
       & > .text {
         ${caption_02('var(--myspec-gray-scalegray-900)')}
         margin-top: -1px;
-        position: relative;
         width: fit-content;
       }
     }
@@ -139,9 +141,7 @@ export const Comment = styled.div`
 export const Reply = styled.div`
   align-items: flex-start;
   background-color: var(--myspec-gray-scalewhite);
-  border-bottom-style: solid;
-  border-bottom-width: 1px;
-  border-color: var(--myspec-gray-scalegray-200);
+  border-bottom: 1px solid var(--myspec-gray-scalegray-200);
   display: inline-flex;
   flex: 0 0 auto;
   flex-direction: column;
@@ -150,68 +150,11 @@ export const Reply = styled.div`
   position: relative;
 
   & > .profile {
-    align-items: flex-start;
-    display: inline-flex;
-    flex: 0 0 auto;
+    ${commonProfileStyle}
     gap: 120px;
-    position: relative;
-    & > .image-name-date {
-      align-items: center;
-      display: inline-flex;
-      flex: 0 0 auto;
-      gap: 12px;
-      justify-content: center;
-      position: relative;
-      & > .image {
-        background-color: var(--myspec-gray-scalegray-300);
-        border-radius: 30px;
-        height: 32px;
-        position: relative;
-        width: 32px;
-      }
-      & > .name-date {
-        position: relative;
-        display: inline-flex;
-        flex-direction: column;
-        align-items: flex-start;
-        flex: 0 0 auto;
 
-        & > .name {
-          ${caption_02('var(--myspec-gray-scalegray-900)')}
-          margin-top: -1px;
-          position: relative;
-          width: 136px;
-        }
-        & > .date {
-          ${caption_03('var(--myspec-gray-scalegray-900)')}
-          position: relative;
-          width: 136px;
-        }
-      }
-    }
     & > .menu {
-      align-items: flex-start;
-      display: inline-flex;
-      flex: 0 0 auto;
-      gap: 10px;
-      padding: 8px;
-      position: relative;
-      & > .meatball {
-        display: flex;
-        flex-direction: column;
-        height: 16px;
-        position: relative;
-        width: 4px;
-        gap: 2px;
-
-        & > .ellipse {
-          background-color: var(--myspec-gray-scalegray-600);
-          border-radius: 2px;
-          height: 4px;
-          position: relative;
-          width: 4px;
-        }
-      }
+      ${commonMenuStyle}
     }
   }
 
@@ -222,42 +165,32 @@ export const Reply = styled.div`
   }
 
   & > .likes-reply {
-    align-items: flex-end;
-    display: inline-flex;
-    flex: 0 0 auto;
+    ${commonLikesReplyStyle}
     gap: 167px;
-    position: relative;
 
     & > .likes {
-      align-items: center;
-      display: inline-flex;
-      flex: 0 0 auto;
+      ${commonLikesReplyStyle}
       gap: 4px;
-      position: relative;
+
       & > .like {
-        align-items: center;
-        display: inline-flex;
-        flex: 0 0 auto;
+        ${commonLikesReplyStyle}
         gap: 6px;
-        position: relative;
+
         & > .text {
           ${body_01('var(--myspec-gray-scalegray-900)')}
           margin-top: -1px;
-          position: relative;
           width: 56px;
         }
       }
     }
+
     & > .reply {
-      align-items: flex-start;
-      display: inline-flex;
-      flex: 0 0 auto;
+      ${commonLikesReplyStyle}
       gap: 8px;
-      position: relative;
+
       & > .text {
         ${caption_02('var(--myspec-gray-scalegray-900)')}
         margin-top: -1px;
-        position: relative;
         width: fit-content;
       }
     }
