263e65444e4cde3678690476fbed21b99a5d2557
daeun lee <comdang23@gmail.com>
2023-11-29 19:42:01 +0900

    [DEL] 불필요한 코드 제거 #NADA-49

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
--- src/components/common/TagButton.js
+++ src/components/common/TagButton.js
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-import { body_01 } from '../../styles/fontStyle';
+import { Button, TextWarpper } from '../../styles/Community/SelectButton';
 
 const TagButton = ({ tag }) => {
   return (
@@ -9,22 +8,3 @@ const TagButton = ({ tag }) => {
   );
 };
 export default TagButton;
-
-export const Button = styled.div`
-  align-items: center;
-  border-radius: 16px;
-  display: inline-flex;
-  overflow: hidden;
-  position: relative;
-  background-color: var(--myspec-gray-scalegray-100);
-  gap: 8px;
-  padding: 4px 12px;
-`;
-
-export const TextWarpper = styled.div`
-  ${body_01('var(--myspec-gray-scalegray-600)')}
-  margin-top: -1px;
-  position: relative;
-  white-space: nowrap;
-  width: fit-content;
-`;
