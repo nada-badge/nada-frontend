21676e154ae45a54c3f694897f60d8b8004672f7
daeun lee <comdang23@gmail.com>
2023-11-30 17:38:42 +0900

    [CHORE] dudgp님의 피드백 반영 #NADA-49

--- src/components/common/menuModalType.js
+++ src/components/common/menuModalType.js
@@ -56,46 +56,11 @@ export function MenuTypeConfig(content) {
   };
 
   const Config = {
-    Report: (
-      <div>
-        {PrintCenteredText({
-          print: '신고하기',
-          act: toReport,
-        })}
-      </div>
-    ),
-    Delete: (
-      <div>
-        {PrintCenteredText({
-          print: '삭제하기',
-          act: toDelete,
-        })}
-      </div>
-    ),
-    Share: (
-      <div>
-        {PrintCenteredText({
-          print: '공유하기',
-          act: toShare,
-        })}
-      </div>
-    ),
-    Update: (
-      <div>
-        {PrintCenteredText({
-          print: '수정하기',
-          act: toUpdate,
-        })}
-      </div>
-    ),
-    Copy: (
-      <div>
-        {PrintCenteredText({
-          print: '댓글 내용 복사하기',
-          act: toCopy,
-        })}
-      </div>
-    ),
+    Report: <PrintCenteredText print={'신고하기'} act={toReport} />,
+    Delete: <PrintCenteredText print={'삭제하기'} act={toDelete} />,
+    Share: <PrintCenteredText print={'공유하기'} act={toShare} />,
+    Update: <PrintCenteredText print={'수정하기'} act={toUpdate} />,
+    Copy: <PrintCenteredText print={'댓글 내용 복사하기'} act={toCopy} />,
   };
 
   return Config[content];
