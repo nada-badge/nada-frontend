5a455268254b94c220af2c1187e27c78b0220765
daeun lee <comdang23@gmail.com>
2023-12-01 10:00:31 +0900

    [FIX] return문 추가를 통해 오류 해결 #NADA-49

--- src/containers/community/postWrite/Image.js
+++ src/containers/community/postWrite/Image.js
@@ -1,26 +1,28 @@
 import { Images } from '../../../styles/Community/PostWriteStyle';
 
 export const Image = () => {
-  <Images>
-    <div className="images">
-      <div className="image">
-        <div className="overlap-group">
-          <div className="rectangle" />
-          <img
-            className="img"
-            alt="Vector"
-            src="https://c.animaapp.com/prsgtMdH/img/vector-12.svg"
-          />
-          <div className="ellipse" />
-          <div className="group">
+  return (
+    <Images>
+      <div className="images">
+        <div className="image">
+          <div className="overlap-group">
+            <div className="rectangle" />
             <img
-              className="vector-2"
+              className="img"
               alt="Vector"
-              src="https://c.animaapp.com/prsgtMdH/img/vector.svg"
+              src="https://c.animaapp.com/prsgtMdH/img/vector-12.svg"
             />
+            <div className="ellipse" />
+            <div className="group">
+              <img
+                className="vector-2"
+                alt="Vector"
+                src="https://c.animaapp.com/prsgtMdH/img/vector.svg"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Images>;
+    </Images>
+  );
 };
