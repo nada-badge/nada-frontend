b949ae507cc7816fd369ca17a787832e5b0df404
daeun lee <comdang23@gmail.com>
2023-12-01 10:00:31 +0900

    [FIX] return문 추가를 통해 오류 해결 #NADA-49

--- src/containers/community/postWrite/Content.js
+++ src/containers/community/postWrite/Content.js
@@ -1,14 +1,16 @@
 import { ContentStyle } from '../../../styles/Community/PostWriteStyle';
 
 export const Content = ({ onChange, inputValue }) => {
-  <ContentStyle>
-    <input
-      className="text"
-      name="content"
-      onChange={onChange}
-      value={inputValue.content}
-      placeholder="내용을 입력하세요"
-      required
-    />
-  </ContentStyle>;
+  return (
+    <ContentStyle>
+      <input
+        className="text"
+        name="content"
+        onChange={onChange}
+        value={inputValue.content}
+        placeholder="내용을 입력하세요"
+        required
+      />
+    </ContentStyle>
+  );
 };
--- src/containers/community/postWrite/FilterBar.js
+++ src/containers/community/postWrite/FilterBar.js
@@ -3,10 +3,12 @@ import { MainCategoryButton } from './MainCategoryButton';
 import { OpenModalButton } from '../../../components/community/PostWrite/openModalButton';
 
 export const FilterBar = () => {
-  <Frame>
-    <MainCategoryButton />
-    <OpenModalButton content="region" />
-    <OpenModalButton content="field" />
-    <OpenModalButton content="category" />
-  </Frame>;
+  return (
+    <Frame>
+      <MainCategoryButton />
+      <OpenModalButton content="region" />
+      <OpenModalButton content="field" />
+      <OpenModalButton content="category" />
+    </Frame>
+  );
 };
