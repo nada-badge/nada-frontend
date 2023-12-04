2f5e6587d2794f12fd2d0e3af31a834a71b791ee
daeun lee <comdang23@gmail.com>
2023-12-04 15:12:28 +0900

    [CHORE] 대소문자 수정 #NADA-49

---
+++ src/components/community/PostWrite/SelectButton.js
@@ -0,0 +1,61 @@
+/** SelectButton  region, field, category 모달 내 개별요소에 대한 버튼 */
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  postWriteSelector,
+  addField,
+  deleteField,
+  setField,
+  initializeForm,
+} from '../../../modules/Community/postWrite';
+import { selectAllConfig } from '../../common/AreaFieldCategoryData';
+import { AttributeInfoButton } from '../../common/AttributeInfoButton';
+
+export const SelectButton = ({ text }) => {
+  const dispatch = useDispatch();
+
+  const [isActive, setIsActive] = useState(false);
+  const form = 'postWriteSelect';
+
+  const modal = useSelector(({ modal }) => modal);
+  const { contentType } = modal;
+  const buttonAll = selectAllConfig(contentType);
+
+  //region, field와 category의 타입차이로 인한 삼항 연산자
+  const setStatus = contentType === 'category' ? setField : addField;
+  const deleteStatus =
+    contentType === 'category' ? initializeForm : deleteField;
+
+  const state = useSelector(postWriteSelector('postWriteSelect', contentType));
+
+  const onClick = () => {
+    const deactivateButton = () => {
+      dispatch(deleteStatus({ form, key: contentType, value: text }));
+      if (state.length === 1) {
+        dispatch(setStatus({ form, key: contentType, value: buttonAll }));
+      }
+    };
+
+    const activateButton = () => {
+      dispatch(setStatus({ form, key: contentType, value: text }));
+      if (contentType !== 'category') {
+        dispatch(deleteField({ form, key: contentType, value: buttonAll }));
+      }
+    };
+
+    isActive ? deactivateButton() : activateButton();
+  };
+
+  useEffect(() => {
+    setIsActive(state.includes(text));
+  }, [state]);
+
+  return (
+    <AttributeInfoButton
+      text={text}
+      onClick={onClick}
+      isActive={isActive}
+      isImg={true}
+    />
+  );
+};
---
+++ src/components/community/comment/replyOutput.js
@@ -0,0 +1,32 @@
+/**ReplyOutPut 답글을 출력하는 컴포넌트 */
+import { ReplyBox } from '../../../styles/Community/CommentStyle';
+import { Profile } from './Profile';
+import { Menu } from './Menu';
+import { Like } from './Like';
+import { Reply } from './Reply';
+
+const ReplyOutPut = ({ reply }) => {
+  const sendReply = () => {
+    //코드 추가 예정
+  };
+
+  const openMenu = () => {
+    //코드 추가 예정
+  };
+
+  return (
+    <ReplyBox>
+      <div className="header">
+        <Profile comment={reply} />
+        <Menu opemMenu={openMenu} />
+      </div>
+      <div className="content">{reply.content}</div>
+      <div className="bottom">
+        <Like />
+        <Reply sendComment={sendReply} />
+      </div>
+    </ReplyBox>
+  );
+};
+
+export default ReplyOutPut;
