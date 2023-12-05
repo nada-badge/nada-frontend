78fb23d486375f29ff0d89672c8c31b1fa957ebd
daeun lee <comdang23@gmail.com>
2023-12-05 13:36:37 +0900

    [FIX] filter와 error 메시지 겹침 현상 해결 #NADA-49

--- src/containers/community/ErrorMessage.js
+++ src/containers/community/ErrorMessage.js
@@ -3,36 +3,40 @@ import { applyFontStyles } from '../../styles/fontStyle';
 
 export const ErrorMessage = () => {
   return (
-    <div>
-      <ErrorTitle>조건에 맞는 게시물이 없습니다</ErrorTitle>
-      <ErrorContent>선택한 필터를 변경해 보세요</ErrorContent>
-    </div>
+    <Error>
+      <div className="ErrorTitle">조건에 맞는 게시물이 없습니다</div>
+      <div className="ErrorContent">선택한 필터를 변경해 보세요</div>
+    </Error>
   );
 };
 
-export const ErrorTitle = styled.div`
-  ${applyFontStyles({
-    font: 'body-02',
-    color: 'var(--myspec-gray-scalegray-600)',
-  })};
-  height: 21px;
-  left: 109px;
-  position: absolute;
-  text-align: center;
-  top: 170px;
-  white-space: nowrap;
-`;
+export const Error = styled.div`
+  position: relative;
 
-export const ErrorContent = styled.div`
-  ${applyFontStyles({
-    font: 'caption-02',
-    color: 'var(--myspec-gray-scalegray-600)',
-  })};
+  & > .ErrorTitle {
+    ${applyFontStyles({
+      font: 'body-02',
+      color: 'var(--myspec-gray-scalegray-600)',
+    })};
+    height: 21px;
+    left: 109px;
+    position: absolute;
+    text-align: center;
+    top: 82px;
+    white-space: nowrap;
+  }
 
-  height: 18px;
-  left: 120px;
-  position: absolute;
-  text-align: center;
-  top: 193px;
-  white-space: nowrap;
+  & > .ErrorContent {
+    ${applyFontStyles({
+      font: 'caption-02',
+      color: 'var(--myspec-gray-scalegray-600)',
+    })};
+
+    height: 18px;
+    left: 120px;
+    position: absolute;
+    text-align: center;
+    top: 105px;
+    white-space: nowrap;
+  }
 `;
