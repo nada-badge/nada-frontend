0c3f02bb606c4214f0bafb39960c4bebce89f9e7
daeun lee <comdang23@gmail.com>
2023-12-05 13:27:28 +0900

    [ADD] 404에러에 따른 retry 설정 #NADA-49

--- src/modules/queries/useListQuery.js
+++ src/modules/queries/useListQuery.js
@@ -1,5 +1,4 @@
-// usePostListQuery.js
-
+/* useListQuery community에 list를 가져오는 쿼리 */
 import { useQuery } from '@tanstack/react-query';
 import client from '../../lib/api/client';
 
@@ -17,6 +16,15 @@ const useListQuery = ({ mainCategory, region, field, category }) => {
       });
 
       return resp.data.posts.reverse();
+    },
+    {
+      retry: (failureCount, error) => {
+        if (error.response && error.response.status === 404) {
+          return false; // 리트라이 하지 않음
+        }
+        // 다른 에러는 기본적으로 리트라이
+        return failureCount < 3; // 여기서는 최대 3번까지 리트라이
+      },
     },
   );
 };
