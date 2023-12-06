dbcb193b7845a3f37cc1b3fdf2205adfcd786833
daeun lee <comdang23@gmail.com>
2023-12-07 00:03:09 +0900

    [REFACT] 댓글창 컴포넌트화 및 svg 적용 #NADA-49

---
+++ src/containers/community/comment/CommentBar.js
@@ -0,0 +1,44 @@
+import classNames from 'classnames';
+import { useState, useEffect } from 'react';
+import { InputBar, Img } from '../../../styles/community/CommentBarStyle';
+
+const CommentBar = ({ inputValue, onChange, sendComment }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setIsActive(!!inputValue);
+  }, [inputValue]);
+
+  return (
+    <InputBar>
+      <div className="input">
+        <div className="text">
+          <input
+            className="wrapper"
+            onChange={onChange}
+            value={inputValue}
+            placeholder="댓글을 입력하세요"
+          />
+        </div>
+      </div>
+      <Img
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 36 36"
+        fill="none"
+        className={classNames({ isActive })}
+        onClick={sendComment}
+      >
+        <rect className="background" fill="#F8F8F8" rx="12" />
+        <path
+          className="arrow"
+          stroke="#D9D9D9"
+          stroke-linecap="round"
+          stroke-width="2"
+          d="m12 17 6-6 6 6m-6-4v12"
+        />
+      </Img>
+    </InputBar>
+  );
+};
+
+export default CommentBar;
