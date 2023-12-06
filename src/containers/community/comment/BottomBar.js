04b145a87e9f20bac296ff50f8ba0b4d35586912
daeun lee <comdang23@gmail.com>
2023-12-07 00:03:55 +0900

    [REFACT] replyNoticeBar 및 commentBar 적용 #NADA-49

--- src/containers/community/comment/BottomBar.js
+++ src/containers/community/comment/BottomBar.js
@@ -1,30 +1,26 @@
 /** BottomBar 댓글 화면에서 보여지는 하단바 */
 import { useState, useEffect } from 'react';
-import classNames from 'classnames';
-//import useComment from '../../../modules/queries/commentQuery';
-//import useReply from '../../../modules/queries/replyQuery';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { PostDetailSelector } from '../../../modules/community/postDetail';
 import { Bar } from '../../../styles/community/CommentBarStyle';
+import CommentBar from './CommentBar';
+import ReplyNoticeBar from './ReplyNoticeBar';
 import usePostQuery from '../../../modules/queries/usePostQuery';
 import preSetForQuery from '../../../components/common/preSetForQuery';
+
 export const BottomBar = () => {
   const content = useSelector(PostDetailSelector('Comment', 'content'));
   const position = useSelector(PostDetailSelector('Comment', 'position'));
   const [inputValue, setInputValue] = useState();
-  const [isActive, setIsActive] = useState(false);
   const { mutate } = usePostQuery();
   const PostDetail = useSelector(({ postdetail }) => postdetail);
   //const sendReply = useReply().mutate;
   const url = preSetForQuery(position, PostDetail).url;
-
+  const isReply = PostDetail.Comment.position === 'reply';
+  console.log('position in bottom : ', PostDetail);
   const onChange = (e) => {
     setInputValue(e.target.value);
   };
-
-  useEffect(() => {
-    setIsActive(!!inputValue);
-  }, [inputValue]);
 
   useEffect(() => {
     setInputValue(content);
@@ -37,19 +33,11 @@ export const BottomBar = () => {
 
   return (
     <Bar>
-      <div className="input">
-        <div className="text">
-          <input
-            className="wrapper"
-            onChange={onChange}
-            value={inputValue}
-            placeholder="댓글을 입력하세요"
-          />
-        </div>
-      </div>
-      <div
-        className={classNames('sendArrow', { isActive })}
-        onClick={sendComment}
+      {isReply && <ReplyNoticeBar name={PostDetail.Comment.userName} />}
+      <CommentBar
+        inputValue={inputValue}
+        onChange={onChange}
+        sendComment={sendComment}
       />
     </Bar>
   );
