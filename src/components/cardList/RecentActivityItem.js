bf40f2a64aa6b8dcf67987da9b8adeede03c86d2
dudgp128 <nbv19@naver.com>
2023-12-11 02:12:33 +0900

    [CSS] CSS 코드 간결하게 수정하기 #NADA-52

--- src/components/cardList/RecentActivityItem.js
+++ src/components/cardList/RecentActivityItem.js
@@ -1,72 +1,7 @@
 import styled from 'styled-components';
-import { subtitle_03, caption_02, caption_01 } from '../../styles/fontStyle';
+import { applyFontStyles } from '../../styles/fontStyle';
 import { Star } from '../activity/Star';
 import { Link } from 'react-router-dom';
-
-const CardContainer = styled.div`
-  min-width: 184px;
-  height: 94px;
-  border-radius: 10px;
-  border: 1px solid #d9d9d9;
-  position: relative;
-
-  & > a {
-    text-decoration: none;
-  }
-
-  & > a > div {
-    width: calc(100% - 20px);
-    height: calc(100% - 20px);
-    position: relative;
-    padding: 10px;
-    text-align: left;
-
-    & > .title {
-      ${subtitle_03('var(--myspec-gray-scalegray-900)')}
-      height: 21px;
-      position: relative;
-    }
-
-    & > .team {
-      ${caption_02('var(--myspec-primaryblue-1)')}
-      height: 14px;
-      position: relative;
-      top: 4px;
-      width: calc(100% - (16px + 14px));
-    }
-
-    & > .group {
-      height: 24px;
-      position: relative;
-      top: 14px;
-      display: flex;
-      width: fit-content;
-
-      & > .Dday {
-        ${caption_01('var(--myspec-gray-scalegray-900)')}
-        height: 17px;
-        position: relative;
-        align-self: center;
-      }
-
-      & > .see {
-        ${caption_01('var(--myspec-gray-scalegray-600)')}
-        height: 17px;
-        position: relative;
-        left: 4px;
-        align-self: center;
-      }
-    }
-
-    & > .star {
-      position: absolute;
-      top: calc(48px + 10px);
-      left: calc(
-        100% - 24px - 10px
-      ); // 전체 넓이 - 아이콘 크기(본인) - paddingLeft
-    }
-  }
-`;
 
 const RecentActivityItem = ({ cards }) => {
   return (
@@ -81,9 +16,7 @@ const RecentActivityItem = ({ cards }) => {
                 <div className="Dday">D-{Dday}</div>
                 <div className="see">조회 {see}</div>
               </div>
-              <div className="star">
-                <Star />
-              </div>
+              <Star className="star" /> {/* ◀ svg 파일 */}
             </div>
           </Link>
         </CardContainer>
@@ -93,3 +26,59 @@ const RecentActivityItem = ({ cards }) => {
 };
 
 export default RecentActivityItem;
+
+const CardContainer = styled.div`
+  box-sizing: border-box;
+  min-width: 184px;
+  height: 94px;
+  border-radius: 10px;
+  border: 1px solid #d9d9d9;
+  position: relative;
+  text-align: left;
+  padding: 10px;
+
+  & > a {
+    text-decoration: none;
+  }
+
+  & > a > div {
+    & > .title {
+      ${applyFontStyles({
+        font: 'subtitle-03',
+      })};
+      position: relative;
+    }
+
+    & > .team {
+      ${applyFontStyles({
+        font: 'caption-02',
+        color: 'var(--myspec-primaryblue-1)',
+      })};
+      position: relative;
+      top: 4px;
+    }
+
+    & > .group {
+      height: 24px;
+      position: relative;
+      top: 14px;
+      display: flex;
+      width: fit-content;
+      gap: 4px;
+      ${applyFontStyles({
+        font: 'caption-01',
+      })};
+
+      & > div {
+        align-self: center;
+      }
+    }
+
+    & > svg {
+      position: relative;
+      top: -12px;
+      left: calc(100% - 24px); // 전체 넓이 - 아이콘 크기(본인)
+      z-index: 2;
+    }
+  }
+`;
