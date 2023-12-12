091b286816103073f7275076b4ae2b9143e716c9
dudgp128 <20181048@sungshin.ac.kr>
2023-12-13 02:52:07 +0900

    [CSS] styled-components를 js Object로 변경하기 #NADA-52
    title은 -webkit을 사용하여 js Object화가 되지 않습니다

--- src/components/cardList/ActivityItem.js
+++ src/components/cardList/ActivityItem.js
@@ -1,59 +1,60 @@
 import styled from 'styled-components';
 import { applyFontStyles } from '../../styles/fontStyle';
 
-const CardContainer = styled.div`
-  border-radius: 10px;
+const Title = styled.p`
+  ${applyFontStyles({ font: 'subtitle-01' })};
+  white-space: normal;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
   position: relative;
-  width: 169px;
-  text-align: left;
-
-  & > .imgContainer {
-    background-image: url(https://generation-sessions.s3.amazonaws.com/34294950d7167123fb2eefcf02c0f744/img/rectangle-4727@2x.png);
-    background-size: 100% 100%;
-    height: 167px;
-    position: relative;
-    width: 167px;
-
-    & > .Dday {
-      box-sizing: border-box;
-      padding: 6px 8px;
-      background-color: #1363ff;
-      border-radius: 8px;
-      height: 26px;
-      left: 115px;
-      position: relative;
-      top: 10px;
-      width: 41px;
-      ${applyFontStyles({ font: 'caption-01', color: '#ffffff' })};
-      text-align: center;
-    }
-  }
-  & > .title {
-    ${applyFontStyles({ font: 'subtitle-01' })};
-    white-space: normal;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    position: relative;
-    box-sizing: border-box;
-    height: 51px;
-    width: 171px;
-    margin: 0;
-    padding: 4px 2px;
-  }
+  box-sizing: border-box;
+  height: 51px;
+  width: 171px;
+  margin: 0;
+  padding: 4px 2px;
 `;
 
 const ActivityItem = ({ cards }) => {
+  const CardContainerStyle = {
+    borderRadius: '10px',
+    position: 'relative',
+    maxWidth: '169px',
+    textAlign: 'left',
+  };
+
+  const imgContainer = {
+    backgroundImage:
+      'url(https://generation-sessions.s3.amazonaws.com/34294950d7167123fb2eefcf02c0f744/img/rectangle-4727@2x.png)',
+    backgroundSize: '100% 100%',
+    aspectRatio: '1 / 1',
+    position: 'relative',
+  };
+
+  const DdayStyle = {
+    ...applyFontStyles({ font: 'caption-01', color: 'white' }),
+    boxSizing: 'border-box',
+    padding: '6px 8px',
+    backgroundColor: '#1363ff',
+    borderRadius: '8px',
+    height: '26px',
+    left: '115px',
+    position: 'relative',
+    top: '10px',
+    width: '41px',
+    textAlign: 'center',
+  };
+
   return (
     <>
       {cards.length &&
         cards.map(({ activityName, Dday = 7 }, idx) => (
-          <CardContainer key={idx}>
-            <div className="imgContainer">
-              <div className="Dday">{`D - ${Dday}`}</div>
+          <div style={CardContainerStyle} key={idx}>
+            <div style={imgContainer}>
+              <div style={DdayStyle}>{`D - ${Dday}`}</div>
             </div>
-            <p className="title">{activityName}</p>
-          </CardContainer>
+            <Title>{activityName}</Title>
+          </div>
         ))}
     </>
   );
