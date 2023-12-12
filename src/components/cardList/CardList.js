491dc1ae495ff9a0ad7231ed402163b2789ed250
dudgp128 <20181048@sungshin.ac.kr>
2023-12-13 01:25:03 +0900

    [CSS] CSS 간결히 수정하기 #NADA-52

--- src/components/cardList/CardList.js
+++ src/components/cardList/CardList.js
@@ -2,46 +2,42 @@ import styled from 'styled-components';
 import { applyFontStyles } from '../../styles/fontStyle';
 
 const CardsContainer = styled.div`
+  box-sizing: border-box;
   box-shadow: 0px 2px 5px #00000005;
   position: relative;
+  padding: 12px 15px; // 커스텀
 
   & > .card-container {
     box-sizing: border-box;
-    width: 375px;
+    width: 100%;
     overflow-x: scroll;
-    padding-bottom: 12px; // 커스텀
-    padding-left: 15px;
   }
 `;
 
 const Title = styled.div`
-  left: 16px;
   position: relative;
-  padding: 16px 0px; // 커스텀
   margin: 0;
   text-align: left;
+  padding-bottom: 12px;
 `;
 
 const CardListWrapper = styled.div`
   width: fit-content;
+  height: fit-content;
   gap: 12px;
   align-items: flex-start;
   display: flex;
 `;
 
 const CardList = ({ title, children, title_font }) => {
+  const titleFont = applyFontStyles({
+    font: title_font || 'subtitle-02',
+    color: '',
+  });
+
   return (
     <CardsContainer>
-      {title && (
-        <Title
-          style={applyFontStyles({
-            font: title_font || 'subtitle-02',
-            color: '',
-          })}
-        >
-          {title}
-        </Title>
-      )}
+      {title && <Title style={titleFont}>{title}</Title>}
       <div className="card-container">
         <CardListWrapper>{children}</CardListWrapper>
       </div>
