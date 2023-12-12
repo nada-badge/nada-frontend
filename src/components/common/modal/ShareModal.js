bb259e61a217e90ace416df2d563f30e3e5d4a8d
daeun lee <comdang23@gmail.com>
2023-12-12 16:50:50 +0900

    [CHORE] filter, usedInModal 폴더 경로 변경 #NADA-49

--- src/components/common/modal/ShareModal.js
+++ src/components/common/modal/ShareModal.js
@@ -1,6 +1,6 @@
 /** ShareModal 공유 방법을 선택하는 모달 */
 import { Layout } from '../../../styles/community/NoticeModalStyle';
-import ModalButton from './ModalButton';
+import ModalButton from '../usedInModal/ModalButton';
 
 const ShareModal = () => {
   return (
