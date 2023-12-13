b560764e0ff6d942924dd49c88c0ca130d2abf98
daeun lee <comdang23@gmail.com>
2023-12-13 23:08:28 +0900

    [ADD] comment page에서 삭제, 신고 시 toast 띄우기 #NADA-49

--- src/components/common/modal/AskModal.js
+++ src/components/common/modal/AskModal.js
@@ -7,9 +7,10 @@ import preSetForQuery from '../preSetForQuery';
 import useDelete from '../../../modules/queries/useDelete';
 import toReport from '../toReport';
 import useReport from '../../../modules/queries/useReport';
+import { Toast } from '../Toast';
 
 const AskModal = () => {
-  const { openModal } = useModal();
+  const { openModal, closeModal } = useModal();
   const { mutate } = useDelete();
   const reportMutate = useReport().mutate;
 
@@ -17,6 +18,7 @@ const AskModal = () => {
   const { contentType, actionType, content, position } = modal;
   const PostDetail = useSelector(({ postdetail }) => postdetail);
   const activity = useSelector(({ activity }) => activity.activities);
+
   const useAct = () => {
     if (actionType === '삭제') {
       const config = preSetForQuery(position, PostDetail, activity);
@@ -32,8 +34,12 @@ const AskModal = () => {
         _id: config.idData,
       });
     }
-
-    openModal({ type: 'NoticeModal', contentType, actionType });
+    if (modal.position === 'comment' || modal.position === 'reply') {
+      Toast({ text: '댓글이 ' + actionType + '되었어요' });
+      closeModal();
+    } else {
+      openModal({ type: 'NoticeModal', contentType, actionType });
+    }
   };
 
   return (
