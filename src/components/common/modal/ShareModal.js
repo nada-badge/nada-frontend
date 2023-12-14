001b5893b9e6a12c3aaf783f26ef5a5736ce3864
daeun lee <comdang23@gmail.com>
2023-12-14 14:17:33 +0900

    [FEAT] 링크 복사하기 기능 구현 #NADA-49

--- src/components/common/modal/ShareModal.js
+++ src/components/common/modal/ShareModal.js
@@ -1,29 +1,43 @@
 /** ShareModal 공유 방법을 선택하는 모달 */
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import useModal from '../usedInModal/useModal';
 import { Layout } from '../../../styles/community/NoticeModalStyle';
 import ModalButton from '../usedInModal/ModalButton';
+import { Toast } from '../Toast';
 
 const ShareModal = () => {
+  const { closeModal } = useModal();
+  const url = window.location.href;
+  console.log('now url : ', url);
+
+  const copyLink = () => {
+    Toast({ text: '게시글 링크가 복사되었어요' });
+    closeModal();
+  };
+
   return (
     <Layout>
       <div className="title">공유하기</div>
       <div className="border-2" />
-      <div className="link">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          fill="none"
-        >
-          <path
-            stroke="#888"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m6 12.001 6-6m-4-3 .463-.536a5 5 0 1 1 7.071 7.072l-.534.464m-5 5-.397.534a5.068 5.068 0 0 1-7.127 0 4.972 4.972 0 0 1 0-7.071L3 8.001"
-          />
-        </svg>
-        <div className="text">링크 복사하기</div>
-      </div>
+      <CopyToClipboard text={url} onCopy={() => copyLink()}>
+        <div className="link">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="none"
+          >
+            <path
+              stroke="#888"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m6 12.001 6-6m-4-3 .463-.536a5 5 0 1 1 7.071 7.072l-.534.464m-5 5-.397.534a5.068 5.068 0 0 1-7.127 0 4.972 4.972 0 0 1 0-7.071L3 8.001"
+            />
+          </svg>
+          <div className="text">링크 복사하기</div>
+        </div>
+      </CopyToClipboard>
       <div className="kakaO">
         <svg
           xmlns="http://www.w3.org/2000/svg"
