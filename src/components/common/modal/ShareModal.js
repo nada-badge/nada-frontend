a1afb703c34c7be7e9d836bebaba7900bf51b86f
daeun lee <comdang23@gmail.com>
2023-12-15 22:46:17 +0900

    [FIX] 링크 복사하기 오류 해결 #NADA-49

--- src/components/common/modal/ShareModal.js
+++ src/components/common/modal/ShareModal.js
@@ -8,7 +8,6 @@ import { Toast } from '../Toast';
 const ShareModal = () => {
   const { closeModal } = useModal();
   const url = window.location.href;
-  console.log('now url : ', url);
 
   const copyLink = () => {
     Toast({ text: '게시글 링크가 복사되었어요' });
@@ -19,23 +18,25 @@ const ShareModal = () => {
     <Layout>
       <div className="title">공유하기</div>
       <div className="border-2" />
-      <div className="link">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          fill="none"
-        >
-          <path
-            stroke="#888"
-            strokeLinecap="round"
-            stroke-linejoin="round"
-            strokeWidth="2"
-            d="m6 12.001 6-6m-4-3 .463-.536a5 5 0 1 1 7.071 7.072l-.534.464m-5 5-.397.534a5.068 5.068 0 0 1-7.127 0 4.972 4.972 0 0 1 0-7.071L3 8.001"
-          />
-        </svg>
-        <div className="text">링크 복사하기</div>
-      </div>
+      <CopyToClipboard text={url} onCopy={() => copyLink()}>
+        <div className="link" onClick={() => copyLink()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="none"
+          >
+            <path
+              stroke="#888"
+              strokeLinecap="round"
+              stroke-linejoin="round"
+              strokeWidth="2"
+              d="m6 12.001 6-6m-4-3 .463-.536a5 5 0 1 1 7.071 7.072l-.534.464m-5 5-.397.534a5.068 5.068 0 0 1-7.127 0 4.972 4.972 0 0 1 0-7.071L3 8.001"
+            />
+          </svg>
+          <div className="text">링크 복사하기</div>
+        </div>
+      </CopyToClipboard>
       <div className="kakaO">
         <svg
           xmlns="http://www.w3.org/2000/svg"
