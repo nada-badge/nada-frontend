37940edc82c3dd5ed5cb262a23c9afc9b99b5747
daeun lee <comdang23@gmail.com>
2023-12-18 16:07:25 +0900

    [CSS] SVG 아이콘 파일 분리 #NADA-49

--- src/components/community/comment/Content.js
+++ src/components/community/comment/Content.js
@@ -1,4 +1,5 @@
 import { applyFontStyles } from '../../../styles/fontStyle';
+import { CautionSvg } from '../../../icon/CautionSvg';
 
 const Content = ({ comment }) => {
   const Layout = {
@@ -16,19 +17,7 @@ const Content = ({ comment }) => {
   if (comment.isDeleted) {
     return (
       <div style={Layout}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="17"
-          fill="none"
-          viewBox="0 0 16 17"
-        >
-          <circle cx="8" cy="8.5" r="7.3" stroke="#BBB" stroke-width="1.4" />
-          <path
-            fill="#BBB"
-            d="m8.886 4.5-.137 5.533H7.401L7.264 4.5h1.622ZM7.2 11.583a.88.88 0 0 1 .886-.906c.468-.005.874.404.874.906 0 .508-.406.917-.874.917a.89.89 0 0 1-.886-.917Z"
-          />
-        </svg>
+        <CautionSvg />
         <div style={DeleteText}>댓글이 삭제되었어요</div>
       </div>
     );
