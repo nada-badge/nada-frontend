122f50176f17e6b9c1ece4f834e71a8c650b7165
daeun lee <comdang23@gmail.com>
2023-12-27 20:43:28 +0900

    [FIX] 구문 오류 해결 #NADA-49

--- src/icon/SkeletonImageSvg.js
+++ src/icon/SkeletonImageSvg.js
@@ -10,9 +10,9 @@ export const SkeletonImageSvg = () => {
       <rect width="72" height="72" fill="#F8F8F8" rx="10" />
       <path
         fill="#D9D9D9"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M50 18c2.2 0 4 1.8 4 4v28c0 2.2-1.8 4-4 4H22c-2.2 0-4-1.8-4-4V22c0-2.2 1.8-4 4-4h28ZM34 45.02 29 39l-7 9h28l-9-12-7 9.02ZM29 33a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
       <circle
         cx="53"
@@ -20,12 +20,12 @@ export const SkeletonImageSvg = () => {
         r="11"
         fill="#888"
         stroke="#F8F8F8"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <path
         stroke="#fff"
-        stroke-linecap="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeWidth="2"
         d="M47 53h12m-6 6V47"
       />
     </svg>
