2cc7a7e16bdf0d1613967cc1e283ff2fa159ac35
dudgp128 <nbv19@naver.com>
2023-12-29 00:09:26 +0900

    [FEAT] 활동 게시글 작성하는 페이지 생성하기 #NADA-52

---
+++ src/pages/activity/ActWritePage.js
@@ -0,0 +1,15 @@
+/** ActWritePage 글 작성을 진행하는 페이지 */
+
+const ActWritePage = () => {
+  return (
+    <div className="pageContainer">
+      {/* 이미지 올리기 */}
+      {/* 제목 + 필터 */}
+      {/* 접수기간 + 기관 + 장소 */}
+      {/* 이미지 리스트 */}
+      {/* 내용 입력하기 */}
+    </div>
+  );
+};
+
+export default ActWritePage;
