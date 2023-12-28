12d2e2dfe4c1718715519256bcbb8e000c624723
dudgp128 <nbv19@naver.com>
2023-12-28 17:15:44 +0900

    [CHORE] 0일때도, '-' 표시하기 #NADA-52

--- src/modules/activity/calculateDday.js
+++ src/modules/activity/calculateDday.js
@@ -2,5 +2,5 @@ export const calculateDday = (endedAt) => {
   const endDate = new Date(endedAt);
   const timeDiff = endDate - new Date();
   const result = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  return result > 0 ? `- ${result}` : `+ ${-result}`;
+  return result >= 0 ? `- ${result}` : `+ ${-result}`;
 };
