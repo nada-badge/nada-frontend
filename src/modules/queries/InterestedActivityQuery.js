29ed5b937bfa85c3441324bd423e81cf25341123
dudgp128 <nbv19@naver.com>
2023-12-29 23:32:47 +0900

    [FIXED] 관심활동 해제 쿼리 수정하기
    axio에서 delete 수행할 경우, { data : {object} } 형태로 감싸 요청의 body에 담아 전송합니다

--- src/modules/queries/InterestedActivityQuery.js
+++ src/modules/queries/InterestedActivityQuery.js
@@ -4,9 +4,10 @@ import client from '../../lib/api/client';
 export const useInterestedMutation = () => {
   return useMutation({
     mutationFn: ({ type, email, _id }) => {
-      console.log(type, email, _id);
       if (type === 'delete') {
-        return client.delete('/calendar/bookmark', { email: email, _id: _id });
+        return client.delete('/calendar/bookmark', {
+          data: { email: email, _id: _id },
+        });
       } else if (type === 'add') {
         return client.post('/calendar/bookmark', { email: email, _id: _id });
       }
