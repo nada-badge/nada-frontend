1a3027c7ac49f2863faf2d8cd77a4b528029e098
daeun lee <comdang23@gmail.com>
2023-12-30 23:39:27 +0900

    [FIX] 이미지 업로드 형식에 맞춰 수정 #NADA-49

--- src/modules/queries/useImageQuery.js
+++ src/modules/queries/useImageQuery.js
@@ -4,11 +4,28 @@ import client from '../../lib/api/client';
 
 const useImageQuery = () => {
   return useMutation({
-    mutationFn: ({ section, file }) =>
-      client.post(`/image`, {
-        section: section,
-        file: file,
-      }),
+    mutationFn: async ({ section, files }) => {
+      const formData = new FormData();
+      formData.append('section', section);
+
+      files.forEach((file) => {
+        formData.append(`file`, file);
+      });
+
+      const { data } = await client.post(`/image`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      return data; // 또는 필요한 데이터를 반환
+    },
+    onSuccess: (data) => {
+      return data;
+    },
+    onError: (error) => {
+      console.log('이미지 불러오기에 실패하였습니다.', error);
+    },
   });
 };
 
