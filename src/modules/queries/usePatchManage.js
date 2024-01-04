24c5c6278e736afdb189515053ff17c28fab483d
daeun lee <comdang23@gmail.com>
2024-01-04 23:52:17 +0900

    [RENAME]module 파일명, 함수명 수정하기 #NADA-69

--- src/modules/queries/usePatchManage.js
+++ src/modules/queries/usePatchManage.js
@@ -1,12 +1,13 @@
-/* ManageActiveQuery 관리자 게시물 등록을 수행하는 쿼리 */
+/* usePatchManage 관리자 게시물 등록을 수행하는 쿼리 */
 import client from '../../lib/api/client';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
-const useSubmit = () => {
+const usePatchManage = () => {
   const navigate = useNavigate();
   return useMutation({
     mutationFn: ({
+      _id,
       activityName,
       groupName,
       field,
@@ -16,7 +17,8 @@ const useSubmit = () => {
       start,
       ended,
     }) =>
-      client.post(`/activity`, {
+      client.patch(`/activity`, {
+        _id: _id,
         activityName: activityName,
         groupName: groupName,
         field: field,
@@ -27,16 +29,16 @@ const useSubmit = () => {
         endedAt: ended,
       }),
     onSuccess: (data) => {
-      console.log('게시물이 등록되었습니다.', data);
+      console.log('게시물이 수정되었습니다.', data);
       alert('등록되었습니다.');
       navigate('/manage/Activity');
     },
     onError: (error) => {
-      console.log('게시물 등록에 실패하였습니다.', error);
+      console.log('게시물 수정에 실패하였습니다.', error);
       alert('실패.');
       navigate('/manage/Activity');
     },
   });
 };
 
-export default useSubmit;
+export default usePatchManage;
