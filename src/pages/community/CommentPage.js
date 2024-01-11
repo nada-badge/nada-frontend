2fdafad96bf9759c02d3d35c84fa2585a8411036
daeun lee <comdang23@gmail.com>
2024-01-11 13:15:13 +0900

    [FEAT] useGetComment 구현 및 적용 #NADA-49

--- src/pages/community/CommentPage.js
+++ src/pages/community/CommentPage.js
@@ -1,6 +1,8 @@
 /** CommentPage 댑글과 답글을 보여주는 페이지 */
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useGetComment } from '../../modules/queries/community/useGetComment';
 import styled from 'styled-components';
 import CommentOutPut from '../../components/community/comment/commentOutput';
 import { CommentList } from '../../styles/community/CommentStyle';
@@ -10,8 +12,9 @@ import { applyFontStyles } from '../../styles/fontStyle';
 
 const CommentPage = () => {
   const dispatch = useDispatch();
-  const PostDetail = useSelector(({ postdetail }) => postdetail.PostDetail);
-  const { comments } = PostDetail;
+  const params = useParams();
+  const { data, isLoading, isError } = useGetComment({ _id: params._id });
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     dispatch(
@@ -23,9 +26,17 @@ const CommentPage = () => {
     );
   }, []);
 
+  useEffect(() => {
+    if (isLoading || isError) {
+      return;
+    }
+
+    setComments(data.comments);
+  }, [data, isLoading, isError]);
+
   return (
     <div>
-      {!PostDetail.comments.length && <Message>댓글을 남겨보세요</Message>}
+      {!comments.length && <Message>댓글을 남겨보세요</Message>}
       <CommentList>
         {comments.map((comment) => (
           <CommentOutPut key={comment._id} comment={comment} />
