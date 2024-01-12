260344177654cc848396fb079325dcecc3544228
dudgp128 <nbv19@naver.com>
2024-01-12 21:13:40 +0900

    [ADD] Align컴포넌트 onClick prop 추가하기 #NADA-60

--- src/components/badge/AlignBox.js
+++ src/components/badge/AlignBox.js
@@ -1,6 +1,6 @@
 import { applyFontStyles } from '../../styles/fontStyle';
 
-export const AlignBox = ({ text }) => {
+export const AlignBox = ({ text, onClick }) => {
   const BoxWrapper = {
     width: 'fit-content',
     display: 'flex',
@@ -20,7 +20,7 @@ export const AlignBox = ({ text }) => {
   );
 
   return (
-    <span style={BoxWrapper}>
+    <span style={BoxWrapper} onClick={onClick}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="11"
