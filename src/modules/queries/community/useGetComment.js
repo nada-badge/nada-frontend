3478e56d9b453c0815808972ca1c501863621519
daeun lee <comdang23@gmail.com>
2024-01-12 18:11:49 +0900

    [REFACT]useGetComment에 enabled속성 추가 #NADA-49

--- src/modules/queries/community/useGetComment.js
+++ src/modules/queries/community/useGetComment.js
@@ -3,8 +3,14 @@ import { useQuery } from '@tanstack/react-query';
 import client from '../../../lib/api/client';
 
 export const useGetComment = ({ _id }) => {
-  return useQuery(['getComment'], async () => {
-    const { data } = await client.get(`/community/comment/list/${_id}`, {});
-    return data;
-  });
+  return useQuery(
+    ['getComment'],
+    async () => {
+      const { data } = await client.get(`/community/comment/list/${_id}`, {});
+      return data;
+    },
+    {
+      enabled: Boolean(_id), // _id 값이 존재하는 경우에만 쿼리 실행
+    },
+  );
 };
