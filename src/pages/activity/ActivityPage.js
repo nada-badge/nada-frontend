30857f67b8c62c0ac2a7ac291d29d4dbddfc14d5
dudgp128 <nbv19@naver.com>
2024-01-12 22:37:20 +0900

    [REFACT] ActivityList 로 분리하기 #NADA-52

--- src/pages/activity/ActivityPage.js
+++ src/pages/activity/ActivityPage.js
@@ -1,17 +1,16 @@
 import CardList from '../../components/cardList/CardList';
 import { SearchInput } from '../../components/search/SearchInput';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { changeBarStatus } from '../../modules/redux/bar';
 import { useDispatch, useSelector } from 'react-redux';
 import RecentActivityItem from '../../components/cardList/RecentActivityItem';
 import Filter from '../../components/common/filter/Filter';
-import ActivityItem from '../../components/cardList/ActivityItem';
-import { FixedSizeGrid as Grid } from 'react-window';
 import { AlignBox } from '../../components/badge/AlignBox';
 import { useNavigate } from 'react-router-dom';
 import { initialized } from '../../modules/search/search';
 import { useActivityList } from '../../modules/queries/activity/useGetActivity';
 import '../../styles/Page.scss';
+import AcitivityList from '../../containers/activity/AcitivityList';
 
 const ActivityPage = () => {
   const dispatch = useDispatch();
@@ -41,27 +40,6 @@ const ActivityPage = () => {
     );
   }, []);
 
-  const Cell = useCallback(
-    ({ columnIndex, key, rowIndex, style }) => {
-      const index = rowIndex * 2 + columnIndex;
-      const activity = activities[index];
-      return (
-        <div
-          style={{
-            ...style,
-            boxSizing: 'border-box',
-            paddingRight: '12px',
-            paddingBottom: '12px',
-          }}
-          key={key}
-        >
-          {activity && <ActivityItem cards={activity} />}
-        </div>
-      );
-    },
-    [activities],
-  );
-
   // 검색 redux 초기화하기
   useEffect(() => {
     dispatch(initialized());
@@ -86,17 +64,7 @@ const ActivityPage = () => {
           <Filter />
           <AlignBox text={'최신 순'} />
         </div>
-        <Grid
-          columnCount={2}
-          columnWidth={182}
-          height={Math.ceil(activities.length / 2) * 222}
-          rowCount={Math.ceil(activities.length / 2)}
-          rowHeight={234}
-          width={375}
-          style={{ boxSizing: 'border-box', padding: '12px' }}
-        >
-          {Cell}
-        </Grid>
+        <AcitivityList activities={activities} isError={isError} />
       </div>
     </div>
   );
