6ed11f9ec7cfbe0420db199dda5537961910e950
dudgp128 <nbv19@naver.com>
2024-01-12 21:30:14 +0900

    [ADD] 지역 필터 적용하기 #NADA-26

--- src/containers/auth/register/RegionForm.js
+++ src/containers/auth/register/RegionForm.js
@@ -1,10 +1,16 @@
 import { authSelector } from '../../../modules/auth';
 import { TitleBox, Explain, LoginBtn } from '../../../styles/Auth';
-import { FilterItems } from '../../../components/common/filter/FilterItems';
+import {
+  FilterItems,
+  Item,
+} from '../../../components/common/filter/FilterItems';
 import { useState } from 'react';
+import { regionData } from '../../../modules/common/AttributeData';
+import { AttributeInfoButton } from '../../../components/common/AttributeInfoButton';
 
 const RegionForm = ({ dispatchField, onSubmit, order, type }) => {
   const [disabled, setDisabled] = useState(true);
+
   return (
     <>
       <TitleBox>
@@ -14,7 +20,11 @@ const RegionForm = ({ dispatchField, onSubmit, order, type }) => {
         <br />
         <Explain>지역을 바탕으로 알맞는 정보를 추천해 드릴게요</Explain>
       </TitleBox>
-      {/* <FilterItems text={'region'} /> */}
+      <Item style={{ paddingTop: '45px' }}>
+        {regionData.map(({ id, text }) => (
+          <AttributeInfoButton text={text} />
+        ))}
+      </Item>
       <LoginBtn form={order} disabled={disabled}>
         <div>다음</div>
       </LoginBtn>
