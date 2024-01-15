4b3882eab28815401a4380cf662c7ee045b4bdc6
daeun lee <comdang23@gmail.com>
2024-01-15 20:11:41 +0900

    [FEAT] DatePicker header 커스텀 #NADA-52

--- src/components/common/modal/CalendarModal.js
+++ src/components/common/modal/CalendarModal.js
@@ -1,45 +1,75 @@
 /** CalendarModal 캘린더를 통해 start, end 날짜 정하는 모달 */
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import '../../../styles/calendar/DatePicker.scss';
+import ko from 'date-fns/locale/ko';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import ModalButtonDiv from '../usedInModal/ModalButtonDiv';
 import styled from 'styled-components';
+import { AngleBracket } from '../../../icon/AngleBracket';
+registerLocale('ko', ko);
 
 const CalendarModal = () => {
   const modal = useSelector(({ modal }) => modal);
-  const [dateSet, setDateSet] = useState({ start: '', end: '' });
-
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
   return (
     <Layout>
       {/* <캘린더 라이브러리를 사용해서 들어가기> */}
       {/* onClick 시 postWrite 리덕스테 값 저장하기*/}
       {/* 확인 시 postWrite submit에 리덕스테 값 저장하기*/}
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView={'dayGridMonth'}
-        headerToolbar={{ start: 'prev', center: 'title', end: 'next' }}
-        height={'254px'}
-        width={'280px'}
-        contentHeight={'40px'}
-        locale={'ko'}
-        //   dayCellContent={(info) => {
-        //     const dayNumber = info.dayNumberText.replace('일', '');
-        //     return info.isToday ? <TodayBox text={dayNumber} /> : dayNumber;
-        //   }}
-        fixedWeekCount={false}
-        //   events={events}
-        //   eventContent={(info) => <EventBox text={info.event.title} />}
-        //   dateClick={(info) => openHandler(info)}
-        // datesSet={(dateInfo) => {
-        //   setDateSet((prevDateSet) => ({
-        //     ...prevDateSet,
-        //     start: dateInfo.start,
-        //     end: dateInfo.end,
-        //   }));
-        //}}
-      />
+      <div>
+        <DatePicker
+          selected={startDate}
+          onChange={onChange}
+          startDate={startDate}
+          endDate={endDate}
+          selectsRange
+          locale={'ko'}
+          showPopperArrow={false}
+          fixedHeight
+          inline
+          renderCustomHeader={({
+            date,
+            decreaseMonth,
+            increaseMonth,
+            prevMonthButtonDisabled,
+            nextMonthButtonDisabled,
+          }) => (
+            <div className="dataPickerHeader">
+              <AngleBracket
+                Direction="left"
+                onClick={decreaseMonth}
+                className="monthButton"
+                disabled={prevMonthButtonDisabled}
+              />
+
+              <div>
+                <span className="month">
+                  {date.getFullYear()}년 {date.getMonth() + 1}월{' '}
+                </span>
+              </div>
+              <div>
+                <AngleBracket
+                  Direction="right"
+                  onClick={increaseMonth}
+                  className="monthButton"
+                  disabled={nextMonthButtonDisabled}
+                />
+              </div>
+            </div>
+          )}
+        />
+      </div>
+      {/** ▼act 추가 */}
       <ModalButtonDiv actText={'확인'} />
     </Layout>
   );
---
+++ src/icon/AngleBracket.js
@@ -0,0 +1,14 @@
+export const AngleBracket = ({ Direction }) => {
+  const result = Direction === 'right' ? 'm1 13 6-6-6-6' : 'M7 1 1 7l6 6';
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="8"
+      height="14"
+      fill="none"
+      viewBox="0 0 8 14"
+    >
+      <path stroke="#1A1A1A" d={result} />
+    </svg>
+  );
+};
