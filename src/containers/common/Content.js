08baf9cc47e34f6612313bc156876d640da53df3
daeun lee <comdang23@gmail.com>
2024-01-17 17:24:43 +0900

    [CHROE] 공용 컴테이너를 common 폴더로 이동 #NADA-52

--- src/containers/common/Content.js
+++ src/containers/common/Content.js
@@ -1,5 +1,5 @@
 /** Content 글작성 페이지에 내용 input을 받는 컴테이너  */
-import { ContentStyle } from '../../../styles/community/PostWriteStyle';
+import { ContentStyle } from '../../styles/community/PostWriteStyle';
 
 export const Content = ({ onChange, inputValue }) => {
   return (
