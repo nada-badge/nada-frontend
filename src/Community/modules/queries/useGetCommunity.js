eb9bff32730dc17bf845ad12c829d07f178c2c9a
dudgp128 <nbv19@naver.com>
2024-01-20 00:19:50 +0900

    [FEAT] 인기 게시글 불러오기 #NADA-43

--- src/Community/modules/queries/useGetCommunity.js
+++ src/Community/modules/queries/useGetCommunity.js
@@ -47,3 +47,15 @@ export const useGetPostList = ({ filter }) => {
     },
   );
 };
+
+// 인기 게시글을 불러오기
+export const useGetTopPost = () => {
+  return useQuery({
+    queryKey: ['getTopPost'],
+    queryFn: async () => {
+      const { data } = await client.get('/community/topPost');
+      return data.posts;
+    },
+    staleTime: 9000000,
+  });
+};
