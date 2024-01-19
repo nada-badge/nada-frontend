d73ea4a5510dc57086f139534daa509d94f4049b
dudgp128 <nbv19@naver.com>
2024-01-20 01:12:18 +0900

    [FEAT] 뱃지 redux로 관리하기 #NADA-60

---
+++ src/Badge/modules/redux/badge.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  name: '',
+};
+
+const badgeSlice = createSlice({
+  name: 'badge',
+  initialState,
+  reducers: {
+    changeField: (state, { payload: { key, value } }) => {
+      state[key] = value;
+    },
+    initialized: () => {
+      return initialState;
+    },
+  },
+});
+
+export default badgeSlice;
+export const { changeField, initialized } = badgeSlice.actions;
