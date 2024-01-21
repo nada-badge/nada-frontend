c1f67340a944348b1f2380db4403893a7693cb92
daeun lee <comdang23@gmail.com>
2024-01-21 21:26:58 +0900

    [REFACT] 뒤로 가기, Toast로 수정 #NADA-52

--- src/Acitivity/modules/queries/usePostActivity.js
+++ src/Acitivity/modules/queries/usePostActivity.js
@@ -2,6 +2,7 @@
 import client from '../../../lib/api/client';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import { Toast } from '../../../components/common/Toast';
 
 const usePostActivity = () => {
   const navigate = useNavigate();
@@ -38,13 +39,13 @@ const usePostActivity = () => {
       }),
     onSuccess: (data) => {
       console.log('게시물이 등록되었습니다.', data);
-      alert('등록되었습니다.');
-      navigate('/manage/Activity');
+      navigate(-1);
+      Toast({ text: '등록되었습니다.' });
     },
     onError: (error) => {
       console.log('게시물 등록에 실패하였습니다.', error);
-      alert('실패.');
-      navigate('/manage/Activity');
+      navigate(-1);
+      Toast({ text: '오류 발생!' });
     },
   });
 };
