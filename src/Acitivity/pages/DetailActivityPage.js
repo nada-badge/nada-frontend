67ff27f5d55a3aa18a8cf4c5c983f9c8ce8d338b
dudgp128 <nbv19@naver.com>
2024-01-26 00:15:11 +0900

    [CHORE] 변경된 속성명 적용하기 (imageUrl -> mainImgUrl) #NADA-52

--- src/Acitivity/pages/DetailActivityPage.js
+++ src/Acitivity/pages/DetailActivityPage.js
@@ -1,12 +1,13 @@
 import ContentContainer from '../container/ContentContainer.js';
 import Thumbnail from '../container/Thumbnail.js';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { changeBarStatus } from '../../Bar/modules/redux/bar.js';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useActivity } from '../modules/queries/useGetActivity.js';
 import { calculateDday } from '../modules/calculateDday.js';
+import { applyFontStyles } from '../../styles/fontStyle.js';
 
 const DetailContainer = styled.div`
   background-color: var(--myspec-gray-scalegray-100);
@@ -17,6 +18,8 @@ const DetailContainer = styled.div`
   width: 375px;
   margin: 0px auto;
   padding-bottom: 88px;
+  text-align: left;
+  ${applyFontStyles({ font: 'body-01' })};
 
   & > div {
     background-color: white;
@@ -25,26 +28,25 @@ const DetailContainer = styled.div`
 
 const DetailActivityPage = () => {
   const params = useParams();
-  const { data, isLoading, isError } = useActivity({ _id: params._id });
-  const [info, setInfo] = useState();
+  const { data } = useActivity({ _id: params._id });
 
   // '최근 본 활동'으로 등록하기
   useEffect(() => {
-    if (info) {
+    if (data) {
       // recentActivitiesMap 불러오기
       const recentActivitiesMap = new Map(
         JSON.parse(localStorage.getItem('recentActivitiesMap')) || [],
       );
 
       // 기존 게시글 삭제하기 (중복제거)
-      recentActivitiesMap.delete(info._id);
+      recentActivitiesMap.delete(data._id);
 
       // 새로운 활동 추가하기
-      recentActivitiesMap.set(info._id, {
-        title: info.activityName,
-        team: info.groupName,
-        Dday: calculateDday(info.endedAt),
-        views: info.views,
+      recentActivitiesMap.set(data._id, {
+        title: data.activityName,
+        team: data.groupName,
+        Dday: calculateDday(data.endedAt),
+        views: data.views,
       });
 
       // 3개 이상이면, 삭제하기
@@ -58,7 +60,7 @@ const DetailActivityPage = () => {
         JSON.stringify([...recentActivitiesMap]),
       );
     }
-  }, [info]);
+  }, [data]);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -71,21 +73,20 @@ const DetailActivityPage = () => {
     );
   }, []);
 
-  useEffect(() => {
-    if (isLoading || isError) {
-      return;
-    }
-
-    setInfo(data);
-  }, [data]);
-
   return (
     <DetailContainer>
-      {info && (
+      {data && (
         <>
-          <Thumbnail info={info} />
-          <ContentContainer info={info} />
-          <img className="img" alt="" src={info.imageUrl} />
+          <Thumbnail info={data} />
+          <ContentContainer info={data} />
+          <img className="img" alt="" src={data.mainImageUrl} />
+          <div>{data.context}</div>
+          {data.extraImageUrl.length !== 0 &&
+            data.extraImageUrl.map((el, index) => (
+              <>
+                <img key={index} className="img" alt="" src={el} />
+              </>
+            ))}
         </>
       )}
     </DetailContainer>
