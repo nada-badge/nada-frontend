980859f6cbc0624153ff2a77b28e0b14d9f4fb34
daeun lee <comdang23@gmail.com>
2024-01-26 21:12:31 +0900

    [ADD] 기관 링크 연결 구현 #NADA-52

--- src/Acitivity/container/ContentContainer.js
+++ src/Acitivity/container/ContentContainer.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { subtitle_03, body_01 } from '../../styles/fontStyle';
+import { AngleBracket } from '../../icon/AngleBracket';
 
 const DetailContent = styled.div`
   text-align: left;
@@ -17,10 +18,20 @@ const DetailContent = styled.div`
     width: 255px;
     ${body_01('#1A1A1A')};
   }
+  & .row {
+    ${body_01('#1A1A1A')};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 4px;
+    & > .gray {
+      ${body_01('#888888')};
+    }
+  }
 `;
 
 const ContentContainer = ({ info }) => {
-  const { startedAt, endedAt, institute, area } = info;
+  const { startedAt, endedAt, institute, instituteURL, area } = info;
 
   const formatDate = (start, end) => {
     let startDate = new Date(start);
@@ -35,7 +46,6 @@ const ContentContainer = ({ info }) => {
       weekday: 'short',
     })})`;
   };
-
   return (
     <>
       <DetailContent>
@@ -45,7 +55,19 @@ const ContentContainer = ({ info }) => {
         </div>
         <div className="team">
           <div>기관</div>
-          <div>{institute}</div>
+          <div className="row">
+            <div className="gray">주최</div>
+            <div>{institute}</div>
+            {instituteURL && (
+              <AngleBracket
+                Direction="right"
+                color="#888"
+                onClick={() => {
+                  window.open(instituteURL);
+                }}
+              />
+            )}
+          </div>
         </div>
         <div className="place">
           <div>장소</div>
