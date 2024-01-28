5628d4113db65aec1d445480fd71ffbfb94348a3
daeun lee <comdang23@gmail.com>
2024-01-28 21:26:10 +0900

    [REFACT] activity 대신 postData로 같이 사용 #NADA-52

--- src/modules/common/getBasicUrl.js
+++ src/modules/common/getBasicUrl.js
@@ -1,8 +1,8 @@
 /**getBasicUrl 신고 외 모달이 열린 위치에 따라 쿼리에 사용할 api, idData를 전처리해주는 컴포넌트*/
 
-const getBasicUrl = ({ position, postData, comment, activity = '' }) => {
+const getBasicUrl = ({ position, postData, comment }) => {
   const positionsConfig = {
-    activity: { url: '/activity', idData: activity._id },
+    activity: { url: '/activity', idData: postData.data._id },
     post: {
       url: '/community/post',
       idData: postData ? postData.data._id : '',
