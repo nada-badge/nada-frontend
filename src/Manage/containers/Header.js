6fa79e4e975253e1f514075ff6aaa3c6e3ed6220
daeun lee <comdang23@gmail.com>
2024-01-31 21:55:00 +0900

    [REFACT] 공용사용이 가능한 header 컴포넌트화 #NADA-69

---
+++ src/Manage/containers/Header.js
@@ -0,0 +1,30 @@
+import { useDispatch } from 'react-redux';
+import { LinkButton } from '../components/LinkButton';
+import { applyFontStyles } from '../../styles/fontStyle';
+import { changeField } from '../../modules/redux/postWrite';
+
+const Header = ({ text, url }) => {
+  const dispatch = useDispatch();
+
+  const header = {
+    padding: '20px',
+    ...applyFontStyles({
+      font: 'title-02',
+      color: 'var(--myspec-gray-scalegray-900)',
+    }),
+  };
+
+  const MoveToWrite = () => {
+    dispatch(changeField({ form: 'method', key: 'isSubmit', value: true }));
+  };
+
+  return (
+    <>
+      <div style={header}>{text}</div>
+      <LinkButton url={'/manage'} text={'뒤로 가기 '} Direction="left" />
+      <LinkButton onClick={MoveToWrite} url={url} text={'게시글 작성하기'} />
+    </>
+  );
+};
+
+export default Header;
--- src/Manage/pages/ActivityManagePage.js
+++ src/Manage/pages/ActivityManagePage.js
@@ -1,17 +1,15 @@
-/* ManageActivityList 관리자가 현재 등록되어있는 activity 리스트를 볼 수 있는 페이지 */
+/* ActivityManagePage 관리자가 현재 등록되어있는 activity 리스트를 볼 수 있는 페이지 */
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { FixedSizeGrid as Grid } from 'react-window';
 import styled from 'styled-components';
-import { applyFontStyles } from '../../styles/fontStyle';
 import { useActivityList } from '../../Acitivity/modules/queries/useGetActivity';
-import { changeField } from '../../modules/redux/postWrite';
 import ActivityItem from '../../Acitivity/components/ActivityItem';
-import { LinkButton } from '../components/LinkButton';
 import { changeBarStatus } from '../../Bar/modules/redux/bar';
 import { TextWarpper } from '../../Community/styles/SelectButton';
+import Header from '../containers/Header';
 
-const ManageActivityList = () => {
+const ActivityManagePage = () => {
   const dispatch = useDispatch();
   const [activities, setActivities] = useState([]);
 
@@ -52,20 +50,9 @@ const ManageActivityList = () => {
     );
   };
 
-  const MoveToWrite = () => {
-    dispatch(changeField({ form: 'method', key: 'isSubmit', value: true }));
-  };
-
   return (
     <LayoutStyle>
-      <div className="header">활동 게시글</div>
-      <LinkButton url={'/manage'} text={'뒤로 가기 '} Direction="left" />
-      <LinkButton
-        onClick={MoveToWrite}
-        url={'/activity/ActWrite'}
-        text={'게시글 작성하기'}
-      />
-
+      <Header text={'활동 게시글'} url={'/activity/ActWrite'} />
       <TextWarpper>신고된 게시글</TextWarpper>
       <Grid
         columnCount={6}
@@ -82,16 +69,11 @@ const ManageActivityList = () => {
   );
 };
 
-export default ManageActivityList;
+export default ActivityManagePage;
 
 export const LayoutStyle = styled.div`
   display: flex;
   flex-direction: column;
   padding: 30px;
   gap: 8px;
-
-  & > .header {
-    padding: 20px;
-    ${applyFontStyles({ font: 'title-02' })};
-  }
 `;
