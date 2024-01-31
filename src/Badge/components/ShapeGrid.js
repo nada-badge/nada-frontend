52c8bbff25d71795006751b022b65beabe23f289
dudgp128 <nbv19@naver.com>
2024-02-01 00:27:43 +0900

    [ADD] 뱃지 이미지 파일 저장 및 불러오기 #NADA-60

--- src/Badge/components/ShapeGrid.js
+++ src/Badge/components/ShapeGrid.js
@@ -1,6 +1,11 @@
-import { useState } from 'react';
 // 뱃지 모양 보여주는 그리드 컴포넌트
 import styled from 'styled-components';
+import badge_01 from '../icon/1.webp';
+import badge_02 from '../icon/2.webp';
+import badge_03 from '../icon/3.webp';
+import badge_04 from '../icon/4.webp';
+import badge_05 from '../icon/5.webp';
+import badge_06 from '../icon/6.webp';
 
 const GridContainer = styled.div`
   margin-top: 20px;
@@ -16,6 +21,7 @@ const GridContainer = styled.div`
     border-radius: 20px;
     background-color: var(--myspec-gray-scalegray-100, #f8f8f8);
     box-sizing: border-box;
+    border: 4px solid rgba(0, 0, 0, 0);
 
     &.active {
       border: 4px solid var(--myspec-primaryblue-1, #1363ff);
@@ -28,19 +34,22 @@ const ShapeGrid = ({ active, onClick }) => {
     onClick(dataset);
   };
 
-  const array = Array.from({ length: 6 }, () => 0);
+  const icon = [badge_01, badge_02, badge_03, badge_04, badge_05, badge_06];
+
   return (
     <GridContainer>
-      {array.map((el, index) => (
+      {icon.map((icon, index) => (
         <div
+          style={{
+            backgroundImage: `url(${icon})`,
+            backgroundSize: 'cover',
+          }}
           className={index === active ? 'active' : ''}
           onClick={(e) => handleClick(e)}
           data-name={'shape'}
           key={index}
           data-value={index}
-        >
-          {el}
-        </div>
+        />
       ))}
     </GridContainer>
   );
