34992fee6bca2e77164dfe7707125054eac9905e
daeun lee <comdang23@gmail.com>
2024-01-31 22:06:53 +0900

    [ADD] filter에 따른 PostList 출력 #NADA-69

--- src/Manage/pages/CommunityManagePage.js
+++ src/Manage/pages/CommunityManagePage.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 import Header from '../containers/Header';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeBarStatus } from '../../Bar/modules/redux/bar';
-
+import PostList from '../../components/common/postList/PostList';
+import { useGetPostList } from '../../Community/modules/queries/useGetCommunity';
+import Category from '../../Community/containers/Category';
+import Filter from '../../components/common/filter/Filter';
 const CommunityManagePage = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(({ filter }) => filter);
+
+  // 서버에서 가져온 query 결과 가져오기
+  const result = useGetPostList({ filter: filter });
 
   useEffect(() => {
     dispatch(
@@ -20,6 +27,9 @@ const CommunityManagePage = () => {
   return (
     <LayoutStyle>
       <Header text={'커뮤니티 게시글'} url={'/community/PostWrite'} />
+      <Category />
+      <Filter />
+      <PostList type={'community'} result={result} />
     </LayoutStyle>
   );
 };
