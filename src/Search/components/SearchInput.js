da1ff18845ad5d5b0071001e03cf36d837a748b9
daeun lee <comdang23@gmail.com>
2024-02-02 00:02:08 +0900

    [ADD] mainCategory 설정 컴포넌트 구현 #NADA-49

--- src/Search/components/SearchInput.js
+++ src/Search/components/SearchInput.js
@@ -3,17 +3,17 @@ import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, searchSelector } from '../modules/redux/search';
 import { SearchGlass } from '../../icon/SearchGlass';
+import { MainCategory } from '../../Modal/styles/ModalStyle';
+import { FilterHandler } from '../../icon/FilterHandler';
 
 const Form = styled.form`
   display: flex;
   box-sizing: border-box;
-  width: 100%;
+  flex: 1 0 auto;
   padding: 8px 12px;
   align-items: flex-start;
   justify-content: space-between;
   flex-shrink: 0;
-  border-radius: 10px;
-  background: var(--myspec-gray-scalegray-100);
 
   & > .Input {
     ${applyFontStyles({
@@ -44,15 +44,33 @@ const Form = styled.form`
   }
 `;
 
-export const SearchInput = ({ onSubmit }) => {
+export const SearchInput = ({ onSubmit, isMaincategory }) => {
   const InputWrapper = {
-    width: '100%',
+    width: '345px',
+    height: '40px',
     display: 'flex',
     boxSizing: 'border-box',
-    padding: '12px 15px',
+    margin: '12px 15px',
     justifyContent: 'center',
     alignItems: 'center',
     flexShrink: 0,
+    borderRadius: '10px',
+    backgroundColor: 'var(--myspec-gray-scalegray-100)',
+  };
+
+  const MainCategoryStyle = {
+    minWidth: '56px',
+    backgroundColor: 'var(--myspec-gray-scalegray-200)',
+    borderRadius: '10px  0px 0px 10px ',
+    display: 'flex',
+    boxSizing: 'border-box',
+    padding: '7px 10px',
+    gap: '4px',
+    alignItems: 'center',
+    ...applyFontStyles({
+      font: 'body-01',
+      color: 'var(--myspec-gray-scalegray-900)',
+    }),
   };
 
   const dispatch = useDispatch();
@@ -67,6 +85,16 @@ export const SearchInput = ({ onSubmit }) => {
 
   return (
     <div style={InputWrapper}>
+      {isMaincategory && (
+        <div style={MainCategoryStyle}>
+          모집
+          <FilterHandler
+            isSmall={true}
+            className="unselected"
+            color={'black'}
+          />
+        </div>
+      )}
       <Form onSubmit={onSubmit} id={'search'}>
         <input
           className="Input"
