77877ab5a047f14640ba17daa3c5cd0b1010a6d8
daeun lee <comdang23@gmail.com>
2024-02-03 19:30:27 +0900

    [ADD] noticeWritePage에 form 구현 #NADA-69

--- src/Manage/pages/NoticeWritePage.js
+++ src/Manage/pages/NoticeWritePage.js
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import Header from '../containers/Header';
 
 const NoticeWritePage = () => {
+  const [input, setInput] = useState({
+    title: '',
+    content: '',
+  });
+
+  const form = {
+    display: 'flex',
+    justifyContent: 'column',
+  };
+
+  const onChange = (event) => {
+    const { value, name } = event.target;
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
   return (
     <LayoutStyle>
       <Header text={'공지사항 게시글'} url={''} />
+      <form style={form}>
+        제목
+        <input
+          id="title"
+          name="title"
+          value={input.title}
+          onChange={onChange}
+        />
+        내용
+        <textarea
+          id="content"
+          name="content"
+          value={input.content}
+          onChange={onChange}
+        />
+        <button>등록</button>
+      </form>
     </LayoutStyle>
   );
 };
