8eff7d398267bf70dc330afdb9b3754ccf8f9ccd
dudgp128 <nbv19@naver.com>
2024-02-04 23:31:36 +0900

    [FIXED] 입력 막힘 해결하기 #NADA-60

--- src/Badge/components/IssueListTeam.js
+++ src/Badge/components/IssueListTeam.js
@@ -1,6 +1,3 @@
-// Redux를 사용해 팀 목록 및 개인 목록을 관리하고,
-// 팀을 입력시, 해당 팀의 개인 목록을 표시하고 추가하는 기능을 제공합니다.
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RightArrowSvg } from '../../icon/Login/RightArrowSvg';
 import { FixedLoginBtn } from '../containers/register/ShapeForm';
@@ -8,7 +5,7 @@ import { TextWithSvg } from '../../styles/Survey';
 import IssueListInputItem from './IssueListInputItem';
 import { addList } from '../modules/redux/badge';
 
-const IssueListTeam = ({ onSubmit, list, setList }) => {
+const IssueListTeam = ({ onSubmit, teamName, setTeamName }) => {
   const issueList = useSelector(({ badge }) => badge.issueList);
   const teams = useSelector(({ badge }) => badge.teams);
 
@@ -30,61 +27,59 @@ const IssueListTeam = ({ onSubmit, list, setList }) => {
     );
   };
 
-  const TeamList = () => {
-    return (
-      <>
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
-          {teams.map((team) => (
-            <TextWithSvg key={team} onClick={() => setList(team)}>
-              <div>{team}</div>
-              <RightArrowSvg width={8} height={16} />
-            </TextWithSvg>
-          ))}
-        </div>
-        <FixedLoginBtn onClick={onSubmit} disabled={false}>
-          <div>다음</div>
-        </FixedLoginBtn>
-      </>
-    );
-  };
-
-  const PeopleList = () => {
-    return (
-      <>
-        <div
-          style={{
-            overflowY: 'scroll',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '24px',
-          }}
-        >
-          {issueList.map((el, index) => (
-            <IssueListInputItem key={index} content={el} index={index} />
-          ))}
-        </div>
-        <FixedLoginBtn
-          onClick={() => {
-            dispatchAddList();
-          }}
-        >
-          <div
-            style={{
-              backgroundColor: 'var(--myspec-gray-scalegray-100)',
-              color: 'var(--myspec-gray-scalegray-900)',
-            }}
-          >
-            정보 추가하기
-          </div>
-        </FixedLoginBtn>
-      </>
-    );
-  };
-
   return (
     <>
-      {list === 'index' && <TeamList />}
-      {list !== 'index' && <PeopleList />}
+      {teamName === '' && (
+        <>
+          <div
+            style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}
+          >
+            {teams.map((team) => (
+              <TextWithSvg
+                onClick={() => {
+                  setTeamName(team);
+                }}
+              >
+                <div>{team}</div>
+                <RightArrowSvg width={8} height={16} />
+              </TextWithSvg>
+            ))}
+          </div>
+          <FixedLoginBtn onClick={onSubmit} disabled={false}>
+            <div>다음</div>
+          </FixedLoginBtn>
+        </>
+      )}
+      {teamName && (
+        <>
+          <div
+            style={{
+              overflowY: 'scroll',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '24px',
+            }}
+          >
+            {issueList.map((el, index) => (
+              <IssueListInputItem key={index} content={el} index={index} />
+            ))}
+          </div>
+          <FixedLoginBtn
+            onClick={() => {
+              dispatchAddList();
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: 'var(--myspec-gray-scalegray-100)',
+                color: 'var(--myspec-gray-scalegray-900)',
+              }}
+            >
+              정보 추가하기
+            </div>
+          </FixedLoginBtn>
+        </>
+      )}
     </>
   );
 };
--- src/Badge/containers/register/PeopleForm.js
+++ src/Badge/containers/register/PeopleForm.js
@@ -12,11 +12,11 @@ const PeopleForm = ({ onSubmit }) => {
 
   // 팀 추가시, 팀 리스트를 보여주는 index 화면인지 아닌지
   // setList로 현재 들어온 팀 소속을 설정해줍니다.
-  const [list, setList] = useState('index');
+  const [teamName, setTeamName] = useState('');
 
   return (
     <>
-      {list === 'index' && (
+      {teamName === '' && (
         <TitleBox>
           발급 받을 사람의
           <br />
@@ -24,14 +24,14 @@ const PeopleForm = ({ onSubmit }) => {
         </TitleBox>
       )}
 
-      {list !== 'index' && <TitleBox>{list}</TitleBox>}
+      {teamName && <TitleBox>{teamName}</TitleBox>}
 
       {/* 팀 추가를 하지 않았을때, */}
       {teams.length === 0 && <PeopleInfoInput onSubmit={onSubmit} />}
 
       {/* 팀 추가를 했을때,  */}
       {teams.length !== 0 && (
-        <IssueListTeam onSubmit={onSubmit} list={list} setList={setList} />
+        <IssueListTeam onSubmit={onSubmit} teamName={teamName} setTeamName={setTeamName} />
       )}
     </>
   );
