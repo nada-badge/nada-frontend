2a72ce859f8fd87361e818ca653442c038e20374
dudgp128 <nbv19@naver.com>
2024-02-04 15:54:32 +0900

    [FEAT] 팀 입력시, 해당 팀의 개인 목록을 표시하는 화면 만들기 #NADA-60

--- src/Badge/components/IssueListTeam.js
+++ src/Badge/components/IssueListTeam.js
@@ -1,24 +1,90 @@
-import { useSelector } from 'react-redux';
+// Redux를 사용해 팀 목록 및 개인 목록을 관리하고,
+// 팀을 입력시, 해당 팀의 개인 목록을 표시하고 추가하는 기능을 제공합니다.
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RightArrowSvg } from '../../icon/Login/RightArrowSvg';
 import { FixedLoginBtn } from '../containers/register/ShapeForm';
 import { TextWithSvg } from '../../styles/Survey';
+import IssueListInputItem from './IssueListInputItem';
+import { addList } from '../modules/redux/badge';
 
-const IssueListTeam = ({ onSubmit }) => {
+const IssueListTeam = ({ onSubmit, list, setList }) => {
+  const issueList = useSelector(({ badge }) => badge.issueList);
   const teams = useSelector(({ badge }) => badge.teams);
+
+  const dispatch = useDispatch();
+
+  const dispatchAddList = () => {
+    dispatch(
+      addList({
+        type: 'issueList',
+        value: {
+          name: '',
+          role: '',
+          birth: '',
+          email: '',
+          number: '',
+          team: '',
+        },
+      }),
+    );
+  };
+
+  const TeamList = () => {
+    return (
+      <>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
+          {teams.map((team) => (
+            <TextWithSvg key={team} onClick={() => setList(team)}>
+              <div>{team}</div>
+              <RightArrowSvg width={8} height={16} />
+            </TextWithSvg>
+          ))}
+        </div>
+        <FixedLoginBtn onClick={onSubmit} disabled={false}>
+          <div>다음</div>
+        </FixedLoginBtn>
+      </>
+    );
+  };
+
+  const PeopleList = () => {
+    return (
+      <>
+        <div
+          style={{
+            overflowY: 'scroll',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '24px',
+          }}
+        >
+          {issueList.map((el, index) => (
+            <IssueListInputItem key={index} content={el} index={index} />
+          ))}
+        </div>
+        <FixedLoginBtn
+          onClick={() => {
+            dispatchAddList();
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: 'var(--myspec-gray-scalegray-100)',
+              color: 'var(--myspec-gray-scalegray-900)',
+            }}
+          >
+            정보 추가하기
+          </div>
+        </FixedLoginBtn>
+      </>
+    );
+  };
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
-        {teams.map((team) => (
-          <TextWithSvg>
-            <div>{team}</div>
-            <RightArrowSvg width={8} height={16} />
-          </TextWithSvg>
-        ))}
-      </div>
-      <FixedLoginBtn onClick={onSubmit} disabled={false}>
-        <div>다음</div>
-      </FixedLoginBtn>
+      {list === 'index' && <TeamList />}
+      {list !== 'index' && <PeopleList />}
     </>
   );
 };
--- src/Badge/containers/register/PeopleForm.js
+++ src/Badge/containers/register/PeopleForm.js
@@ -1,25 +1,38 @@
+// 발급 받을 사람의 정보를 입력하는 화면
+// 팀 추가시, 해당 팀 목록을 표시하고 선택하면 동적으로 화면 업데이트
 import { TitleBox } from '../../../styles/Survey';
 import { useSelector } from 'react-redux';
 
 import PeopleInfoInput from '../../components/PeopleInfoInput';
 import IssueListTeam from '../../components/IssueListTeam';
+import { useState } from 'react';
 
 const PeopleForm = ({ onSubmit }) => {
   const teams = useSelector(({ badge }) => badge.teams);
 
+  // 팀 추가시, 팀 리스트를 보여주는 index 화면인지 아닌지
+  // setList로 현재 들어온 팀 소속을 설정해줍니다.
+  const [list, setList] = useState('index');
+
   return (
     <>
-      <TitleBox>
-        발급 받을 사람의
-        <br />
-        정보를 입력해 주세요
-      </TitleBox>
+      {list === 'index' && (
+        <TitleBox>
+          발급 받을 사람의
+          <br />
+          정보를 입력해 주세요
+        </TitleBox>
+      )}
+
+      {list !== 'index' && <TitleBox>{list}</TitleBox>}
 
       {/* 팀 추가를 하지 않았을때, */}
       {teams.length === 0 && <PeopleInfoInput onSubmit={onSubmit} />}
 
       {/* 팀 추가를 했을때,  */}
-      {teams.length !== 0 && <IssueListTeam onSubmit={onSubmit} />}
+      {teams.length !== 0 && (
+        <IssueListTeam onSubmit={onSubmit} list={list} setList={setList} />
+      )}
     </>
   );
 };
