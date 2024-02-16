31ce296b8f7c21b58d6d1601e6f9a63f9c41e7ae
daeun lee <comdang23@gmail.com>
2024-02-16 12:42:16 +0900

    [FIX] 공지사항페이지에서 postData 초기화 #NADA-69

--- src/Manage/pages/NoticeManagePage.js
+++ src/Manage/pages/NoticeManagePage.js
@@ -5,11 +5,12 @@ import { changeBarStatus } from '../../Bar/modules/redux/bar';
 import Header from '../containers/Header';
 import { useGetNoticeList } from '../modules/queries/useGetNotice';
 import { NoticeContent } from '../components/NoticeContent';
+import { initializeForm } from '../../modules/redux/postData';
 
 const NoticeManagePage = () => {
   const dispatch = useDispatch();
   const { data } = useGetNoticeList();
-  console.log('notice data : ', data);
+
   useEffect(() => {
     dispatch(
       changeBarStatus({
@@ -18,6 +19,7 @@ const NoticeManagePage = () => {
         isShowBottom: false,
       }),
     );
+    dispatch(initializeForm());
   }, []);
 
   return (
